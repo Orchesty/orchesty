a82c626f724293c5b5e24a30cc357e4431a4f1da
Radek Bruha <bruha.r@hanaboso.com>
2017-08-11 10:03:05 +0200

    Create UserBundle

---
+++ lib-symfony3/src/HbPFUserBundle/HbPFUserBundle.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\HbPFUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HbPFUserBundle
+ *
+ * @package Hanaboso\PipesFramework\HbPFUserBundle
+ */
+class HbPFUserBundle extends Bundle
+{
+
+}
