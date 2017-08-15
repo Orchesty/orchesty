d18cba4f0a00c1426e16de802c382352f6d04c4d
Venca Krecl <venca.email@gmail.com>
2017-08-15 14:39:21 +0200

    Bundles: add prepend action to register extension

--- lib-symfony3/src/HbPFCommonsBundle/DependencyInjection/HbPFCommonsExtension.php
+++ lib-symfony3/src/HbPFCommonsBundle/DependencyInjection/HbPFCommonsExtension.php
@@ -4,6 +4,7 @@ namespace Hanaboso\PipesFramework\HbPFCommonsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -12,8 +13,20 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class HbPFCommonsExtension extends Extension
+class HbPFCommonsExtension extends Extension implements PrependExtensionInterface
 {
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/prepend-config'));
+        $loader->load('fos_rest.yml');
+        $loader->load('sensio_framework_extra.yml');
+    }
 
     /**
      * @param array            $configs
