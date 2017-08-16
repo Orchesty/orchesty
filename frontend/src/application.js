510902040ca6e251acab5a15ca403ee5c4fb0cf0
Michal Bartl <michal.bartl@gmail.com>
2017-08-16 16:34:56 +0200

    frontend: webpack configs

--- frontend/src/application.js
+++ frontend/src/application.js
@@ -6,25 +6,25 @@ import FlusanecMenu from 'flusanec/src/view_models/menu'
 import ContainerType from './view_models/content/container_type';
 import WindowManager from 'flusanec/src/view_models/window/window_manager';
 import SinglePageManager from './_flusanec/view_models/page/single_page_manager';
-import TestController from './controllers/test_controller';
+//import TestController from './controllers/test_controller';
 import TopologyDataSource from './models/topology/topology_data_source';
 import TopologyManager from './models/topology/topology_manager';
 import TopologyController from './controllers/topology_controller';
-import FakeServer from './services/servers/fake_server';
+//import FakeServer from './services/servers/fake_server';
 import ApiGatewayServer from './services/servers/api_gateway_server';
 import OpenFileDialogService from './services/files/open_file_dialog_service';
 import NotifyService from './services/notification/notify_service';
 import ContextMenuService from 'flusanec/src/services/context_menu/context_menu_service';
 
-import server_fake_data from './data/fake/server.json';
-import server_fake_files from './data/fake/files_loader';
+//import server_fake_data from './data/fake/server.json';
+//import server_fake_files from './data/fake/files_loader';
 
 
 class Application{
   constructor(){
     this._name = 'Pipes';
-    this._server = new FakeServer(server_fake_data, server_fake_files);
-    //this._server = new ApiGatewayServer('http://pipes-example:81/gateway/api');
+    //this._server = new FakeServer(server_fake_data, server_fake_files);
+    this._server = new ApiGatewayServer('http://pipes-example:81/gateway/api');
     this._notifyService = new NotifyService(10000);
     this._containerType = new ContainerType(ContainerType.PAGE);
     this._menu = new FlusanecMenu.Menu([], true);
@@ -49,7 +49,7 @@ class Application{
         topologyManager: new TopologyManager(this._dataSourceContainer)
       }
     };
-    this._testController = new TestController(this._contextServices);
+   // this._testController = new TestController(this._contextServices);
     this._topologyController = new TopologyController(this._contextServices);
   }
   
