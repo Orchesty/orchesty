6ec36f0a4a0b6fa355553ac9cf99eec8844f0308
David Horacek <horacek.d@hanaboso.com>
2017-08-18 15:42:54 +0200

    Process dto

---
+++ src/HbPFConnectorBundle/Handler/ConnectorHandler.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: david.horacek
+ * Date: 8/18/17
+ * Time: 2:03 PM
+ */
+
+namespace Hanaboso\PipesFramework\HbPFConnectorBundle\Handler;
+
+use Hanaboso\PipesFramework\Connector\ConnectorInterface;
+use Hanaboso\PipesFramework\HbPFConnectorBundle\Loaders\ConnectorLoader;
+use Hanaboso\PipesFramework\HbPFConnectorBundle\Manager\ConnectorManager;
+
+/**
+ * Class ConnectorHandler
+ *
+ * @package Hanaboso\PipesFramework\HbPFConnectorBundle\Handler
+ */
+class ConnectorHandler
+{
+
+    /**
+     * @var ConnectorManager
+     */
+    private $connManager;
+
+    /**
+     * @var ConnectorLoader
+     */
+    private $loader;
+
+    /**
+     * ConnectorHandler constructor.
+     *
+     * @param ConnectorManager $connManager
+     * @param ConnectorLoader  $loader
+     */
+    function __construct(ConnectorManager $connManager, ConnectorLoader $loader)
+    {
+        $this->connManager = $connManager;
+        $this->loader      = $loader;
+    }
+
+    /**
+     * @param string   $id
+     * @param string   $token
+     * @param string[] $data
+     *
+     * @return string[]
+     */
+    public function processEvent(string $id, string $token, array $data): array
+    {
+        /** @var ConnectorInterface $conn */
+        $conn = $this->loader->getConnector($id);
+        $res  = $this->connManager->processEvent($conn, $data);
+
+        return $res;
+    }
+
+}
