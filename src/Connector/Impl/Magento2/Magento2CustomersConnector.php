99bb369e2330977f793823c9fa90d41e5f3c1106
Radek Jirsa <jirsa.r@hanaboso.com>
2017-08-21 09:37:42 +0200

    Resolved PHPStan errors

--- src/Connector/Impl/Magento2/Magento2CustomersConnector.php
+++ src/Connector/Impl/Magento2/Magento2CustomersConnector.php
@@ -11,7 +11,7 @@ namespace Hanaboso\PipesFramework\Connector\Impl\Magento2;
 
 use Hanaboso\PipesFramework\Commons\Message\MessageInterface;
 use Hanaboso\PipesFramework\Commons\Process\ProcessDto;
-use Hanaboso\PipesFramework\HbPFConnectorBundle\Exception\ConnectorException;
+use Hanaboso\PipesFramework\Connector\Exception\ConnectorException;
 
 /**
  * Class Magento2CustomersConnector
