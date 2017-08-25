f5df1449fcc0905f007f59ce373a569cb051990a
Radek Bruha <bruha.r@hanaboso.com>
2017-08-25 13:43:42 +0200

    Create MapperBundle

---
+++ src/HbPFMapperBundle/Controller/MapperController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\HbPFMapperBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\FOSRestController;
+
+/**
+ * Class MapperController
+ *
+ * @package Hanaboso\PipesFramework\HbPFMapperBundle\Controller
+ *
+ * @Route(service="hbpf.mapper.controller.mapper")
+ */
+class MapperController extends FOSRestController
+{
+
+}
---
+++ src/HbPFMapperBundle/Handler/MapperHandler.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\HbPFMapperBundle\Handler;
+
+/**
+ * Class MapperHandler
+ *
+ * @package Hanaboso\PipesFramework\HbPFMapperBundle\Handler
+ */
+class MapperHandler
+{
+
+}
