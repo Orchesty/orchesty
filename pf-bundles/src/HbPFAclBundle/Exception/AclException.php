28e5adbb7629b9347657ca0184f64f76f1555433
David Horacek <horacek.d@hanaboso.com>
2017-08-29 14:08:29 +0200

    ACL bundle

---
+++ pf-bundles/src/HbPFAclBundle/Exception/AclException.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\HbPFAclBundle\Exception;
+
+use Hanaboso\PipesFramework\Commons\Exception\PipesFrameworkException;
+
+/**
+ * Class AclException
+ *
+ * @package Hanaboso\PipesFramework\HbPFAclBundle\Exception
+ */
+final class AclException extends PipesFrameworkException
+{
+
+    protected const OFFSET = 1900;
+
+}
