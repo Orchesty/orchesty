19b288ba47911bcb75ae14f4f0f48316e0a25f4a
Radek Bruha <bruha.r@hanaboso.com>
2017-08-30 08:26:16 +0200

    AclBundle: Add ProviderInterface and DatabaseProvider

--- src/Acl/Repository/GroupRepository.php
+++ src/Acl/Repository/GroupRepository.php
@@ -3,6 +3,9 @@
 namespace Hanaboso\PipesFramework\Acl\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
+use Doctrine\ODM\MongoDB\Query\Query;
+use Hanaboso\PipesFramework\Acl\Document\Group;
+use Hanaboso\PipesFramework\User\Document\UserInterface;
 
 /**
  * Class GroupRepository
@@ -11,5 +14,22 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
  */
 class GroupRepository extends DocumentRepository
 {
+
+    /**
+     * @param UserInterface $user
+     *
+     * @return Group[]
+     */
+    public function getUserGroups(UserInterface $user): array
+    {
+        /** @var Query $query */
+        $query = $this->createQueryBuilder()
+            ->field('users')
+            ->includesReferenceTo($user)
+            ->getQuery()
+            ->execute();
+
+        return $query->toArray();
+    }
 
 }
---
+++ tests/Integration/Acl/Provider/Impl/DatabaseProviderTest.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration\Acl\Provider\Impl;
+
+use Hanaboso\PipesFramework\Acl\Document\Group;
+use Hanaboso\PipesFramework\Acl\Document\Rule;
+use Hanaboso\PipesFramework\Acl\Provider\Impl\DatabaseProvider;
+use Hanaboso\PipesFramework\User\Document\User;
+use Tests\DatabaseTestCaseAbstract;
+
+/**
+ * Class DatabaseProviderTest
+ *
+ * @package Tests\Integration\Acl\Provider\Impl
+ */
+class DatabaseProviderTest extends DatabaseTestCaseAbstract
+{
+
+    /**
+     *
+     */
+    public function testGetRules(): void
+    {
+        /** @var DatabaseProvider $databaseProvider */
+        $databaseProvider = $this->container->get('hbpf.acl.provider.database');
+
+        $ruleOne = (new Rule)->setResource('R1');
+        $this->persistAndFlush($ruleOne);
+        $ruleTwo = (new Rule)->setResource('R2');
+        $this->persistAndFlush($ruleTwo);
+        $ruleThree = (new Rule)->setResource('R3');
+        $this->persistAndFlush($ruleThree);
+        $ruleFour = (new Rule)->setResource('R4');
+        $this->persistAndFlush($ruleFour);
+
+        $groupOne = (new Group())
+            ->setName('G1')
+            ->addRule($ruleOne)
+            ->addRule($ruleThree);
+        $ruleOne->setGroup($groupOne);
+        $ruleThree->setGroup($groupOne);
+        $this->persistAndFlush($groupOne);
+
+        $groupTwo = (new Group())
+            ->setName('G1')
+            ->addRule($ruleTwo)
+            ->addRule($ruleFour);
+        $ruleTwo->setGroup($groupTwo);
+        $ruleFour->setGroup($groupTwo);
+        $this->persistAndFlush($groupTwo);
+
+        $user = (new User())->setEmail('user@example.com');
+        $this->persistAndFlush($user);
+
+        $groupOne->addUser($user);
+        $groupTwo->addUser($user);
+
+        $this->dm->flush();
+
+        $rules = $databaseProvider->getRules($user);
+
+        $this->assertEquals(4, count($rules));
+        $this->assertEquals($ruleOne->getResource(), $rules[0]->getResource());
+        $this->assertEquals($ruleOne->getGroup()->getName(), $rules[0]->getGroup()->getName());
+        $this->assertEquals(1, count($rules[0]->getGroup()->getUsers()));
+        $this->assertEquals(
+            $ruleOne->getGroup()->getUsers()[0]->getEmail(),
+            $rules[0]->getGroup()->getUsers()[0]->getEmail()
+        );
+    }
+
+}
