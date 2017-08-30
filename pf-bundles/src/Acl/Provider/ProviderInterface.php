78baf82c1b9728d6c82fee82244c298cc6f67965
Radek Bruha <bruha.r@hanaboso.com>
2017-08-30 08:26:16 +0200

    AclBundle: Add ProviderInterface and DatabaseProvider

---
+++ pf-bundles/src/Acl/Provider/ProviderInterface.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\Acl\Provider;
+
+use Hanaboso\PipesFramework\User\Document\UserInterface;
+
+/**
+ * Interface ProviderInterface
+ *
+ * @package Hanaboso\PipesFramework\Acl\Provider
+ */
+interface ProviderInterface
+{
+
+    /**
+     * @param UserInterface $user
+     *
+     * @return array
+     */
+    public function getRules(UserInterface $user): array;
+
+}
