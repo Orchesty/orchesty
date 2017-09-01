1ad170b79ee0f417009808f0c78c6f91eaa7b4f7
Stanislav Kundrat <kundrat.s@hanaboso.com>
2017-09-01 14:25:36 +0200

    added mongo config

---
+++ src/Commons/Topology/TopologyRepository.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\Commons\Topology;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+
+/**
+ * Class TopologyRepository
+ *
+ * @package Hanaboso\PipesFramework\Commons\Topology
+ */
+class TopologyRepository extends DocumentRepository
+{
+
+}
