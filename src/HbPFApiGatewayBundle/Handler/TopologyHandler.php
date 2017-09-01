80b9bb4eabe3af13acef949683399d13fd0991cc
Stanislav Kundrat <kundrat.s@hanaboso.com>
2017-09-01 16:30:54 +0200

    configurator - topology controller actions #wip

--- src/HbPFApiGatewayBundle/Handler/TopologyHandler.php
+++ src/HbPFApiGatewayBundle/Handler/TopologyHandler.php
@@ -2,6 +2,11 @@
 
 namespace Hanaboso\PipesFramework\HbPFApiGatewayBundle\Handler;
 
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Hanaboso\PipesFramework\Commons\DatabaseManager\DatabaseManagerLocator;
+use Hanaboso\PipesFramework\Commons\Topology\Document\Topology;
+use Hanaboso\PipesFramework\Commons\Topology\TopologyRepository;
+
 /**
  * Class TopologyHandler
  *
@@ -11,21 +16,65 @@ class TopologyHandler
 {
 
     /**
+     * @var DocumentRepository|TopologyRepository
+     */
+    private $topologyRepository;
+
+    /**
+     * TopologyHandler constructor.
+     *
+     * @param DatabaseManagerLocator $dml
+     */
+    public function __construct(DatabaseManagerLocator $dml)
+    {
+        $dm                       = $dml->getDm();
+        $this->topologyRepository = $dm->getRepository(Topology::class);
+    }
+
+    /**
      * @param null $limit
      * @param null $offset
      * @param null $orderBy
      */
     public function getTopologies($limit = NULL, $offset = NULL, $orderBy = NULL): void
     {
+        $sort = '';
 
+        $topologies = $this->topologyRepository->findBy([], $sort, $limit, $offset);
+
+        $data           = [];
+        $data['total']  = '';
+        $data['limit']  = '';
+        $data['count']  = '';
+        $data['offset'] = '';
+
+        foreach ($topologies as $topology) {
+            // TODO $data['items'][] = '';
+        }
+
+        return $data;
     }
 
     /**
      * @param string $id
+     *
+     * @return array
      */
-    public function getTopology(string $id): void
+    public function getTopology(string $id): array
     {
+        /** @var Topology $topology */
+        $topology = $this->topologyRepository->find($id);
 
+        $data = [];
+        if ($topology) {
+            $data['_id']    = $topology->getId();
+            $data['name']   = $topology->getName();
+            $data['descr']  = $topology->getDescr();
+            $data['status'] = $topology->getStatus();
+            $data['nodes']  = $topology->getNodes();
+        }
+
+        return $data;
     }
 
     /**
