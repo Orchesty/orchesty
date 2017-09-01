2ac863888227924eb27b8b32608b3dd3e62ee4cf
Radek Bruha <bruha.r@hanaboso.com>
2017-09-01 11:09:58 +0200

    TableParserBundle: Add Controller Tests

--- src/HbPFTableParserBundle/Controller/ApiController.php
+++ src/HbPFTableParserBundle/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace Hanaboso\PipesFramework\HbPFTableParserBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\Route;
 use FOS\RestBundle\Controller\FOSRestController;
+use Hanaboso\PipesFramework\Commons\Exception\FileStorageException;
 use Hanaboso\PipesFramework\HbPFTableParserBundle\Handler\TableParserHandler;
 use Hanaboso\PipesFramework\HbPFTableParserBundle\Handler\TableParserHandlerException;
 use Hanaboso\PipesFramework\Parser\Exception\TableParserException;
@@ -11,12 +12,13 @@ use Hanaboso\PipesFramework\Utils\ControllerUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ApiController
  *
  * @package Hanaboso\PipesFramework\HbPFTableParserBundle\Controller
+ *
+ * @Route(service="hbpf.parser.table.controller.api")
  */
 class ApiController extends FOSRestController
 {
@@ -37,94 +39,72 @@ class ApiController extends FOSRestController
     }
 
     /**
-     * @Route("/api/parser/{type}/to/json", requirements={"type"="\w+"})
+     * @Route("/api/parser/{type}/to/json", requirements={"type": "\w+"})
      * @Method("POST")
      *
      * @param Request $request
      *
-     * @return Response
+     * @return JsonResponse
      *
      */
-    public function toJsonAction(Request $request): Response
+    public function toJsonAction(Request $request): JsonResponse
     {
-        $response = new JsonResponse();
         try {
-            $data = $this->tableParserHandler->parseToJson($request->request->all());
-            $response->setStatusCode(200);
-        } catch (TableParserHandlerException $e) {
-            $data = ControllerUtils::createExceptionData($e);
-            $response->setStatusCode(500);
+            return new JsonResponse($this->tableParserHandler->parseToJson($request->request->all()));
+        } catch (TableParserHandlerException | FileStorageException $e) {
+            return new JsonResponse(ControllerUtils::createExceptionData($e), 500);
         }
-
-        return $response->setData($data);
     }
 
     /**
-     * @Route("/api/parser/{type}/to/json/test", requirements={"type"="\w+"})
+     * @Route("/api/parser/{type}/to/json/test", requirements={"type": "\w+"})
      * @Method("POST")
      *
-     * @return Response
+     * @return JsonResponse
      *
      */
-    public function toJsonTestAction(): Response
+    public function toJsonTestAction(): JsonResponse
     {
-        $response = new JsonResponse();
-        $data     = '';
         try {
-            $this->tableParserHandler->parseToJsonTest();
-            $response->setStatusCode(200);
-        } catch (TableParserException $e) {
-            $data = ControllerUtils::createExceptionData($e);
-            $response->setStatusCode(500);
+            return new JsonResponse($this->tableParserHandler->parseToJsonTest());
+        } catch (TableParserHandlerException | FileStorageException $e) {
+            return new JsonResponse(ControllerUtils::createExceptionData($e), 500);
         }
-
-        return $response->setData($data);
     }
 
     /**
-     * @Route("/api/parser/json/to/{type}, requirements={"type"="\w+"})
+     * @Route("/api/parser/json/to/{type}", requirements={"type": "\w+"})
      * @Method("POST")
      *
      * @param Request $request
      * @param string  $type
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function fromJsonAction(Request $request, string $type): Response
+    public function fromJsonAction(Request $request, string $type): JsonResponse
     {
-        $response = new JsonResponse();
         try {
-            $data = $this->tableParserHandler->parseFromJson($type, $request->request->all());
-            $response->setStatusCode(200);
-        } catch (TableParserException $e) {
-            $data = ControllerUtils::createExceptionData($e);
-            $response->setStatusCode(500);
+            return new JsonResponse($this->tableParserHandler->parseFromJson($type, $request->request->all()));
+        } catch (TableParserHandlerException | TableParserException | FileStorageException $e) {
+            return new JsonResponse(ControllerUtils::createExceptionData($e), 500);
         }
-
-        return $response->setData($data);
     }
 
     /**
-     * @Route("/api/parser/json/to/{type}/test, requirements={"type"="\w+"})
+     * @Route("/api/parser/json/to/{type}/test", requirements={"type": "\w+"})
      * @Method("POST")
      *
      * @param string $type
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function fromJsonTestAction(string $type): Response
+    public function fromJsonTestAction(string $type): JsonResponse
     {
-        $response = new JsonResponse();
-        $data     = '';
         try {
-            $this->tableParserHandler->parseFromJsonTest($type);
-            $response->setStatusCode(200);
-        } catch (TableParserException $e) {
-            $data = ControllerUtils::createExceptionData($e);
-            $response->setStatusCode(500);
+            return new JsonResponse($this->tableParserHandler->parseFromJsonTest($type));
+        } catch (TableParserHandlerException | TableParserException | FileStorageException $e) {
+            return new JsonResponse(ControllerUtils::createExceptionData($e), 500);
         }
-
-        return $response->setData($data);
     }
 
 }
