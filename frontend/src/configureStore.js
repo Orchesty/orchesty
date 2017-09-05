8fdf989f22885dd76192126e992eb30dcafd8ec7
Michal Bartl <michal.bartl@gmail.com>
2017-09-05 10:24:37 +0200

    added redux-devtools-extension

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(thunkMiddleware))(createStore);
 
 export default function (initialState) {
   return createStoreWithMiddleware(rootReducer, initialState);
