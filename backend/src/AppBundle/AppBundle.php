e5224916850e6c8a22073d10cffd648d0300bc51
David Horacek <horacek.d@hanaboso.com>
2017-09-06 12:03:04 +0200

    Backend app

---
+++ backend/src/AppBundle/AppBundle.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class AppBundle
+ *
+ * @package AppBundle
+ */
+class AppBundle extends Bundle
+{
+
+}
