6eb8c84753858e3158dc3463ab7f3c50744117bc
Radek Bruha <bruha.r@hanaboso.com>
2017-09-13 08:14:38 +0200

    ApiGatewayBundle: NodeController route returns all nodes with exclude option

---
+++ src/Commons/Node/NodeReduction.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\Commons\Node;
+
+/**
+ * Class NodeReduction
+ *
+ * @package Hanaboso\PipesFramework\Commons\Node
+ */
+final class NodeReduction
+{
+
+    /**
+     * @var string[]
+     */
+    public static $typeExclude = [];
+
+}
