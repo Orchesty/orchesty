f1df42e0eaa00556ef1b841d286f46652ac425d4
Pavel Severyn <severyn.p@hanaboso.com>
2017-09-17 21:26:42 +0200

    Python: metrics change row format

--- lib-python/hb_metrics/tests/test_metrics.py
+++ lib-python/hb_metrics/tests/test_metrics.py
@@ -11,20 +11,20 @@ class MetricsTests(unittest.TestCase):
         mock_sender.get_hostname.return_value = 'foo'
         mock_sender.send.return_value = 100
         self.metrics = Metrics(mock_sender)
-
+    
     def test_send(self):
         self.assertEquals(self.metrics.send({}), 100)
-
+    
     @mock.patch('time.time')
     def test_get_message(self, mock_time):
         mock_time.return_value = 1502891211.420394
         for fields, host, result in self.provider_get_message():
             self.assertEquals(self.metrics._get_message(fields, host), result)
-
+    
     def test_get_prefix(self):
         for host, res in self.provider_get_prefix():
             self.assertEquals(res, self.metrics._get_prefix(host))
-
+    
     @staticmethod
     def provider_get_prefix():
         data = (
@@ -32,29 +32,34 @@ class MetricsTests(unittest.TestCase):
             ('localhost', 'python-service,name=localhost,host=localhost'),
             (None, 'python-service,name=foo,host=foo'),
         )
-
+        
         for host, result in data:
             yield host, result
-
+    
     @staticmethod
     def provider_get_message():
         data = (
             (
                 {},
                 'server',
-                'python-service,name=server,host=server  1502891211420000000'
+                'python-service,name=server,host=server 1502891211420000000'
             ),
             (
-                {'key': 'value'},
+                {'boolean': True, 'string': 'text', 'integer': 12, 'bool': False},
                 'localhost',
-                'python-service,name=localhost,host=localhost key=value 1502891211420000000'
+                'python-service,name=localhost,host=localhost integer=12,boolean=true,bool=false,string="text" 1502891211420000000'
+            ),
+            (
+                {'key': 'value', 'number': 0, 'string': '1234'},
+                'localhost',
+                'python-service,name=localhost,host=localhost number=0,key="value",string="1234" 1502891211420000000'
             ),
             (
                 {'key': 'value', 'key2': 'value2'},
                 None,
-                'python-service,name=foo,host=foo key2=value2,key=value 1502891211420000000'
+                'python-service,name=foo,host=foo key2="value2",key="value" 1502891211420000000'
             ),
         )
-
+        
         for fields, host, result in data:
             yield fields, host, result
