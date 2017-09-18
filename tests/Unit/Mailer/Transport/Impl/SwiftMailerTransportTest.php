4cfd466e7edb97873eb276f408a2783bc6f187f8
Stanislav Kundrat <kundrat.s@hanaboso.com>
2017-09-18 17:13:40 +0200

    added attachments to swift mailer

--- tests/Unit/Mailer/Transport/Impl/SwiftMailerTransportTest.php
+++ tests/Unit/Mailer/Transport/Impl/SwiftMailerTransportTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Unit\Mailer\Transport\Impl;
 
+use Hanaboso\PipesFramework\Commons\FileStorage\Document\File;
+use Hanaboso\PipesFramework\Commons\FileStorage\Dto\FileStorageDto;
+use Hanaboso\PipesFramework\Commons\FileStorage\FileStorage;
+use Hanaboso\PipesFramework\Mailer\MessageBuilder\Impl\GenericMessageBuilder\GenericContentAttachment;
+use Hanaboso\PipesFramework\Mailer\MessageBuilder\Impl\GenericMessageBuilder\GenericFsAttachment;
 use Hanaboso\PipesFramework\Mailer\MessageBuilder\Impl\GenericMessageBuilder\GenericTransportMessage;
 use Hanaboso\PipesFramework\Mailer\Transport\Impl\SwiftMailerTransport;
 use Monolog\Logger;
@@ -30,8 +35,19 @@ class SwiftMailerTransportTest extends TestCase
         $logger = $this->createPartialMock(Logger::class, ['info']);
         $logger->method('info')->willReturn(1);
 
+        $attach1 = new GenericContentAttachment('hello', 'text/plain', 'hello.txt');
+        $attach2 = new GenericFsAttachment('123abc', 'text/plain', 'hello.txt');
+
+        /** @var PHPUnit_Framework_MockObject_MockObject|FileStorage $fakeStorage */
+        $fakeStorage = $this->createPartialMock(FileStorage::class, ['getFileDocument', 'getFileStorage']);
+        $fakeStorage->method('getFileDocument')->willReturn(new File());
+        $fakeStorage->method('getFileStorage')->willReturn(new FileStorageDto(new File(), ''));
+
         $message = new GenericTransportMessage('no-reply@test.com', 'no-reply@test.com', 'Subject', 'Content');
-        $mailer  = new SwiftMailerTransport($fakeMailer);
+        $message->addContentAttachment($attach1);
+        $message->addFileStorageAttachment($attach2);
+
+        $mailer = new SwiftMailerTransport($fakeMailer, $fakeStorage);
         $mailer->setLogger($logger);
         $mailer->send($message);
     }
