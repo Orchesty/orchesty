5aa461df8eb3ce22e81e976ca1e8b75000fad9e8
Lukas Hlavac <hlavac.l@hanaboso.com>
2017-09-18 14:35:02 +0200

    BPMN: run time for cron node

---
+++ src/views/elements/bpmn/custom-modeler/PropertiesPanel/parts/CronTimeProps.js
@@ -0,0 +1,38 @@
+import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
+import cmdHelper from 'bpmn-js-properties-panel/lib/helper/CmdHelper';
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+import cronParser from 'cron-parser';
+
+export default function (group, element, translate) {
+  if (element.pipesType !== 'cron') {
+    return;
+  }
+
+  group.entries.push(
+    entryFactory.validationAwareTextField({
+      id: 'cronTime',
+      label: 'Cron time',
+      description: 'eg. */2 * * * *',
+      modelProperty: 'cronTime',
+      validate: (element, values) => {
+        try {
+          cronParser.parseExpression(values.cronTime);
+        } catch (err) {
+          return {
+            cronTime: err.message
+              .replace('undefinedundefinedundefined', '\'\'')
+              .replace('undefined', 'unexpected')
+          };
+        }
+
+        return {};
+      },
+      getProperty: function (element) {
+        return getBusinessObject(element).get('cronTime');
+      },
+      setProperty: function (element, properties) {
+        return cmdHelper.updateProperties(element, properties);
+      },
+    })
+  );
+}
