81e87647d73e01a26c26385873bfa7c7127109a4
David Horacek <horacek.d@hanaboso.com>
2017-09-21 09:49:40 +0200

    SystemInstall document

---
+++ clients/clever-connectors/src/AppBundle/Document/SystemInstall.php
@@ -0,0 +1,157 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Document;
+
+use CleverConnectors\AppBundle\Document\Traits\IdTrait;
+use DateTime;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * Class SystemInstall
+ *
+ * @package CleverConnectors\AppBundle\Document
+ *
+ * @ODM\Document(repositryClass="CleverConnectors\AppBundle\Repository\SystemInstallRepository")
+ */
+class SystemInstall
+{
+
+    use IdTrait;
+
+    /**
+     * @var string
+     *
+     * @ODM\Field(type="string")
+     * @ODM\Index()
+     */
+    protected $user;
+
+    /**
+     * @var string
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $token;
+
+    /**
+     * @var string
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $system;
+
+    /**
+     * @var bool
+     *
+     * @ODM\Field(type="boolean")
+     */
+    protected $synchronized;
+
+    /**
+     * @var DateTime
+     *
+     * @ODM\Field(type="date")
+     */
+    protected $created;
+
+    /**
+     * @return string
+     */
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string $user
+     *
+     * @return SystemInstall
+     */
+    public function setUser(string $user): SystemInstall
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return SystemInstall
+     */
+    public function setToken(string $token): SystemInstall
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSystem(): string
+    {
+        return $this->system;
+    }
+
+    /**
+     * @param string $system
+     *
+     * @return SystemInstall
+     */
+    public function setSystem(string $system): SystemInstall
+    {
+        $this->system = $system;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSynchronized(): bool
+    {
+        return $this->synchronized;
+    }
+
+    /**
+     * @param bool $synchronized
+     *
+     * @return SystemInstall
+     */
+    public function setSynchronized(bool $synchronized): SystemInstall
+    {
+        $this->synchronized = $synchronized;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param DateTime $created
+     *
+     * @return SystemInstall
+     */
+    public function setCreated(DateTime $created): SystemInstall
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+}
---
+++ clients/clever-connectors/src/AppBundle/Repository/SystemInstallRepository.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Repository;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+
+/**
+ * Class SystemInstallRepository
+ *
+ * @package CleverConnectors\AppBundle\Repository
+ */
+class SystemInstallRepository extends DocumentRepository
+{
+
+}
