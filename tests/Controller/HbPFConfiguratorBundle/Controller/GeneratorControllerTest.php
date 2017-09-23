94dfd53afe72d09570b65223f429f8885afdb50f
Pavel Severyn <severyn.p@hanaboso.com>
2017-09-23 13:38:29 +0200

    PHP: Migrate topology generator to configutaro bundle, test

---
+++ tests/Controller/HbPFConfiguratorBundle/Controller/GeneratorControllerTest.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Pavel Severyn
+ * Date: 23.9.17
+ * Time: 13:02
+ */
+
+namespace Tests\Controller\HbPFConfiguratorBundle\Controller;
+
+use Hanaboso\PipesFramework\HbPFConfiguratorBundle\Handler\GeneratorHandler;
+use Tests\ControllerTestCaseAbstract;
+
+/**
+ * Class GeneratorControllerTest
+ *
+ * @package Tests\Controller\HbPFConfiguratorBundle\Controller
+ */
+class GeneratorControllerTest extends ControllerTestCaseAbstract
+{
+
+    /**
+     * @covers       GeneratorController::generateAction()
+     */
+    public function testGenerateActionTrue(): void
+    {
+        $handler = $this->createMock(GeneratorHandler::class);
+        $handler->method('generateTopology')->willReturn(TRUE);
+
+        $this->client->getContainer()->set('hbpf.handler.generator_handler', $handler);
+
+        $this->client->request(
+            'GET',
+            '/topology/generate/123ABC',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            ''
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    /**
+     * @covers       GeneratorController::generateAction()
+     */
+    public function testGenerateActionFalse(): void
+    {
+        $handler = $this->createMock(GeneratorHandler::class);
+        $handler->method('generateTopology')->willReturn(FALSE);
+
+        $this->client->getContainer()->set('hbpf.handler.generator_handler', $handler);
+
+        $this->client->request(
+            'GET',
+            '/topology/generate/123ABC',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            ''
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals($response->getStatusCode(), 400);
+    }
+
+}
