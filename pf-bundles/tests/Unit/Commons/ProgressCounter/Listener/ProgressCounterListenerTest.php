ebb850dbc1d583d29b0eb217d52b35d29a12aa58
Pavel Severyn <severyn.p@hanaboso.com>
2017-10-03 12:30:06 +0200

    Php: ProgressCounter - redis

---
+++ pf-bundles/tests/Unit/Commons/ProgressCounter/Listener/ProgressCounterListenerTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Pavel Severyn
+ * Date: 3.10.17
+ * Time: 22:39
+ */
+
+namespace Tests\Unit\Commons\ProgressCounter\Listener;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ProgressCounterListenerTest
+ *
+ * @package Tests\Unit\Commons\ProgressCounter\Listener
+ */
+class ProgressCounterListenerTest extends TestCase
+{
+
+    //TODO:
+
+}
