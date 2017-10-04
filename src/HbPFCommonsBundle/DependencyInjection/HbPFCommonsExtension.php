b073e717c745b897723799f552b8ce09cd9e7c76
Radek Jirsa <jirsa.r@hanaboso.com>
2017-10-04 14:44:40 +0200

    FosRest: config update

--- src/HbPFCommonsBundle/DependencyInjection/HbPFCommonsExtension.php
+++ src/HbPFCommonsBundle/DependencyInjection/HbPFCommonsExtension.php
@@ -26,7 +26,6 @@ class HbPFCommonsExtension extends Extension implements PrependExtensionInterfac
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/prepend-config'));
         $loader->load('parameters.yml');
         $loader->load('monolog.yml');
-        $loader->load('fos_rest.yml');
         $loader->load('sensio_framework_extra.yml');
 
         $container->setParameter('src_dir', __DIR__ . '/../..');
