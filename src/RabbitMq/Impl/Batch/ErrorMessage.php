055f4cdd179b89c18d772bf6cce350f8eeb164bb
Venca Krecl <venca.email@gmail.com>
2017-10-04 09:32:18 +0200

    BatchAsync: add success and error message

---
+++ src/RabbitMq/Impl/Batch/ErrorMessage.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: venca
+ * Date: 10/4/17
+ * Time: 8:22 AM
+ */
+
+namespace Hanaboso\PipesFramework\RabbitMq\Impl\Batch;
+
+/**
+ * Class ErrorMessage
+ *
+ * @package Hanaboso\PipesFramework\RabbitMq\Impl\Batch
+ */
+class ErrorMessage
+{
+
+    /**
+     * @var int
+     */
+    private $code = 0;
+
+    /**
+     * @var string
+     */
+    private $status = 'SUCCESS';
+
+    /**
+     * @var string
+     */
+    private $message = '';
+
+    /**
+     * @var string
+     */
+    private $detail = '';
+
+    /**
+     * ErrorMessage constructor.
+     *
+     * @param int    $code
+     * @param string $status
+     * @param string $message
+     */
+    public function __construct(int $code, string $status, string $message = '')
+    {
+        $this->code    = $code;
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return ErrorMessage
+     */
+    public function setMessage(string $message): ErrorMessage
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDetail(): string
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @param string $detail
+     *
+     * @return ErrorMessage
+     */
+    public function setDetail(string $detail): ErrorMessage
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+}
