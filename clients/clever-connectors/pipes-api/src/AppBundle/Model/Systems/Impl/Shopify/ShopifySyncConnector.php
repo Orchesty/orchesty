51f17cca623d2a8da85db2b66b79aad6792a9ddf
Radek Jirsa <jirsa.r@hanaboso.com>
2017-10-06 17:42:22 +0200

    wip

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Shopify/ShopifySyncConnector.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radek.jirsa
+ * Date: 6.10.17
+ * Time: 17:36
+ */
+
+namespace CleverConnectors\AppBundle\Model\Systems\Impl\Shopify;
+
+class ShopifySyncConnector
+{
+
+}
