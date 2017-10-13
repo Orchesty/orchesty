ded8299d1514ff9b8257a51614c38cf37513e504
David Horacek <horacek.d@hanaboso.com>
2017-10-13 09:01:28 +0200

    ProgressCounterListener

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Listeners/ProgressCounterListener.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Listeners;
+
+use CleverConnectors\AppBundle\Exceptions\CleverConnectorsException;
+use Hanaboso\PipesFramework\Commons\ProgressCounter\ProgressCounterService;
+use Hanaboso\PipesFramework\Configurator\Event\ProcessStatusEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class ProgressCounterListener
+ *
+ * @package CleverConnectors\AppBundle\Listeners
+ */
+class ProgressCounterListener implements EventSubscriberInterface
+{
+
+    /**
+     * @var ProgressCounterService
+     */
+    private $counter;
+
+    /**
+     * StatusServiceListener constructor.
+     *
+     * @param ProgressCounterService $counter
+     */
+    function __construct(ProgressCounterService $counter)
+    {
+        $this->counter = $counter;
+    }
+
+    /**
+     * @param ProcessStatusEvent $ev
+     *
+     * @throws CleverConnectorsException
+     */
+    public function updateStatus(ProcessStatusEvent $ev): void
+    {
+        $data = $ev->getData();
+        if (empty($data['process_id'] ?? '') || empty($data['status'] ?? '')) {
+            throw new CleverConnectorsException(
+                'Missing message\'s content in ProcessStatusEvent [process_id].',
+                CleverConnectorsException::MISSING_DATA
+            );
+        }
+
+        $this->counter->setStatus($data['process_id'], $data['status']);
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProcessStatusEvent::PROCESS_FINISHED => 'updateStatus',
+        ];
+    }
+
+}
