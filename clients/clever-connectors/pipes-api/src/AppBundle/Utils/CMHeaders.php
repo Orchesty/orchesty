40cb6058ddb67be80966dec456a1ca2f5ac9c722
Radek Jirsa <jirsa.r@hanaboso.com>
2017-10-13 14:06:15 +0200

    Added CMHeaders util

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Utils/CMHeaders.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: radek.jirsa
+ * Date: 13.10.17
+ * Time: 13:57
+ */
+
+namespace CleverConnectors\AppBundle\Utils;
+
+use Hanaboso\PipesFramework\Commons\Utils\PipesHeaders;
+
+/**
+ * Class CMHeaders
+ *
+ * @package CleverConnectors\AppBundle\Utils
+ */
+class CMHeaders extends PipesHeaders
+{
+
+    public const SYSTEM_KEY = 'system_key';
+    public const GUID       = 'guid';
+    public const TOKEN      = 'token';
+
+}
