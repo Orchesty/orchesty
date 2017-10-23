4a83d7077e0f91a42fb6212938fcaefcfbabcde8
Venca Krecl <venca.email@gmail.com>
2017-10-23 14:00:34 +0200

    StreamDemo: add server actions

--- clients/clever-connectors/cc-api-demo/app/presenters/StreamPresenter.php
+++ clients/clever-connectors/cc-api-demo/app/presenters/StreamPresenter.php
@@ -2,6 +2,12 @@
 
 namespace App\Presenters;
 
+use Bunny\Client;
+use CmStream\Subscriber;
+use Nette\Application\Responses\JsonResponse;
+use Nette\Forms\Form;
+use Nette\Http\Request;
+
 /**
  * Class StreamPresenter
  *
@@ -11,12 +17,19 @@ class StreamPresenter extends BasePresenter
 {
 
     /**
+     * @var Subscriber
+     */
+    private $subscriber;
+
+    /**
      * StreamPresenter constructor.
      *
+     * @param Subscriber $subscriber
      */
-    public function __construct()
+    public function __construct(Subscriber $subscriber)
     {
         parent::__construct();
+        $this->subscriber = $subscriber;
     }
 
     /**
@@ -25,6 +38,57 @@ class StreamPresenter extends BasePresenter
     public function renderDefault()
     {
         $this->template->host = $this->context->getParameters()['ws']['host'];
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function actionSubscription(Request $request)
+    {
+        $data = json_decode($request->getRawBody(), TRUE);
+
+        $token = $this->subscriber->subscribe($data['userId'], $data['groups']);
+
+        return new JsonResponse(['token' => $token]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function actionUnsubscription(Request $request)
+    {
+        $data = json_decode($request->getRawBody(), TRUE);
+
+        $this->subscriber->unsubscribe($data['token']);
+
+        return new JsonResponse([]);
+    }
+
+    /**
+     * @param Form $form
+     */
+    public function processPublishForm(Form $form)
+    {
+        $data = $form->getValues(TRUE);
+
+        $client = new Client($this->context->getParameters()['rabbit-mq']);
+        $client->channel()
+            ->publish(
+                json_encode([
+                    'event'   => $data['event'],
+                    'content' => $data['content'],
+                    'groups'  => explode(',', $data['groups']),
+                ]),
+                [
+                    'content-type' => 'application/json',
+                ],
+                '',
+                'stream'
+            );
     }
 
 }
