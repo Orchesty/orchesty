b6b702d7ca5e556e2a80ee01d564ef063a502bea
Radek Jirsa <jirsa.r@hanaboso.com>
2017-10-24 15:22:38 +0200

    Added CMEventHandler

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Controller/CMEventController.php
@@ -0,0 +1,92 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: radek.jirsa
+ * Date: 24.10.17
+ * Time: 15:06
+ */
+
+namespace CleverConnectors\AppBundle\Controller;
+
+use CleverConnectors\AppBundle\Handler\CMEventHandler;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class CMEventController
+ *
+ * @package CleverConnectors\AppBundle\Controller
+ *
+ * @Route(service="cc.cm_event.controller")
+ */
+class CMEventController extends FOSRestController
+{
+
+    /**
+     * @var CMEventHandler
+     */
+    private $handler;
+
+    /**
+     * CMEventController constructor.
+     *
+     * @param CMEventHandler $handler
+     */
+    public function __construct(CMEventHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * @Route("/event/create/{userId}")
+     * @Method("POST")
+     *
+     * @param Request $request
+     * @param string  $userId
+     *
+     * @return Response
+     */
+    public function createAction(Request $request, string $userId): Response
+    {
+        $this->handler->createEvent();
+
+        return new Response([], 200);
+    }
+
+    /**
+     * @Route("/event/unsubscribe/{userId}")
+     * @Method("POST")
+     *
+     * @param Request $request
+     * @param string  $userId
+     *
+     * @return Response
+     */
+    public function unsubscribeAction(Request $request, string $userId): Response
+    {
+        $this->handler->unsubscribeEvent();
+
+        return new Response([], 200);
+    }
+
+    /**
+     * @Route("/event/hard_bounce/{userId}")
+     * @Method("POST")
+     *
+     * @param Request $request
+     * @param string  $userId
+     *
+     * @return Response
+     */
+    public function HardBounceAction(Request $request, string $userId): Response
+    {
+        $this->handler->hardBounceEvent();
+
+        return new Response([], 200);
+    }
+
+}
