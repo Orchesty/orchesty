839967937d9a19ed9b6d61065b6a493138acacd3
David Horacek <horacek.d@hanaboso.com>
2017-10-25 09:36:25 +0200

    Added tests

---
+++ clients/clever-connectors/pipes-api/tests/Unit/AppBundle/Utils/TopologyNameUtilsTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\AppBundle\Utils;
+
+use CleverConnectors\AppBundle\Document\SystemInstall;
+use CleverConnectors\AppBundle\Utils\TopologyNameUtils;
+use Tests\KernelTestCaseAbstract;
+
+/**
+ * Class TopologyNameUtilsTest
+ *
+ * @package Tests\Unit\AppBundle\Utils
+ */
+class TopologyNameUtilsTest extends KernelTestCaseAbstract
+{
+
+    /**
+     *
+     */
+    public static function testGetSyncName(): void
+    {
+        self::assertEquals('systemkey-sync-subscribers', TopologyNameUtils::getSyncName((new SystemInstall())->setSystem('systemkey')));
+    }
+
+    /**
+     *
+     */
+    public static function testGetEventName(): void
+    {
+        $systemInstall = (new SystemInstall())->setSystem('syskey');
+        self::assertEquals('syskey-' . SystemInstall::EVENT_HARD_BOUNCE . '-event', TopologyNameUtils::getEventName($systemInstall, SystemInstall::EVENT_HARD_BOUNCE));
+        self::assertEquals('syskey-' . SystemInstall::EVENT_CREATE . '-event', TopologyNameUtils::getEventName($systemInstall, SystemInstall::EVENT_CREATE));
+    }
+
+    /**
+     *
+     */
+    public static function testGetCustomEventName(): void
+    {
+        $systemInstall = (new SystemInstall())->setSystem('syss')->setUser('us');
+
+        self::assertEquals('us-syss-' . SystemInstall::EVENT_HARD_BOUNCE . '-event', TopologyNameUtils::getCustomEventName($systemInstall, SystemInstall::EVENT_HARD_BOUNCE));
+        self::assertEquals('us-syss-' . SystemInstall::EVENT_CREATE . '-event', TopologyNameUtils::getCustomEventName($systemInstall, SystemInstall::EVENT_CREATE));
+    }
+
+}
