783d724111688c07f8b2c7425feb649bbf980c18
David Horacek <horacek.d@hanaboso.com>
2017-11-07 16:43:20 +0100

    Abstract system

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/Plugins/PluginSystemAbstract.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Model\Plugins;
+
+use CleverConnectors\AppBundle\Document\SystemInstall;
+use CleverConnectors\AppBundle\Enum\SystemTypeEnum;
+use CleverConnectors\AppBundle\Model\CMEvents\CMEventSystemInterface;
+use CleverConnectors\AppBundle\Model\CMEvents\Traits\CMEventSystemTrait;
+use CleverConnectors\AppBundle\Model\Form\Field;
+use CleverConnectors\AppBundle\Model\Form\Form;
+use CleverConnectors\AppBundle\Model\Requester\RequesterInterface;
+use CleverConnectors\AppBundle\Model\Systems\Authorizations\AuthorizationInterface;
+use CleverConnectors\AppBundle\Model\Systems\Authorizations\Traits\AuthorizationTrait;
+use Hanaboso\PipesFramework\Commons\Transport\Curl\Dto\RequestDto;
+
+/**
+ * Class PluginSystemAbstract
+ *
+ * @package CleverConnectors\AppBundle\Model\Plugins
+ */
+abstract class PluginSystemAbstract implements AuthorizationInterface, CMEventSystemInterface
+{
+
+    use AuthorizationTrait;
+    use CMEventSystemTrait;
+
+    /**
+     * @param SystemInstall $systemInstall
+     *
+     * @return bool
+     */
+    public function isAuthorized(SystemInstall $systemInstall): bool
+    {
+        return TRUE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorizationType(): string
+    {
+        return self::BASIC;
+    }
+
+    /**
+     * @param SystemInstall $systemInstall
+     *
+     * @return RequesterInterface|null
+     */
+    public function getCMEventRequester(SystemInstall $systemInstall): ?RequesterInterface
+    {
+        return NULL;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return SystemTypeEnum::PLUGIN;
+    }
+
+    /**
+     * @param SystemInstall $systemInstall
+     * @param string        $method
+     *
+     * @return RequestDto
+     */
+    public function getRequestDto(SystemInstall $systemInstall, string $method): RequestDto
+    {
+        return new RequestDto($method, $systemInstall->getSettings()[SystemInstall::SYSTEM_URL]);
+    }
+
+    /**
+     * @param SystemInstall $systemInstall
+     *
+     * @return array
+     */
+    public function getSettingFields(SystemInstall $systemInstall): array
+    {
+        $field1 = new Field(
+            Field::CHECKBOX,
+            SystemInstall::EVENT_CREATE,
+            'Create event',
+            $systemInstall->isEventCreate()
+        );
+
+        $field2 = new Field(
+            Field::CHECKBOX,
+            SystemInstall::EVENT_UNSUBSCRIBE,
+            'Unsubscribe event',
+            $systemInstall->isEventUnsubscribe()
+        );
+
+        $field3 = new Field(
+            Field::CHECKBOX,
+            SystemInstall::EVENT_HARD_BOUNCE,
+            'Hard bounce event',
+            $systemInstall->isEventHardBounce()
+        );
+
+        $form = new Form();
+        $form
+            ->addField($field1)
+            ->addField($field2)
+            ->addField($field3);
+
+        return $form->toArray();
+    }
+
+}
