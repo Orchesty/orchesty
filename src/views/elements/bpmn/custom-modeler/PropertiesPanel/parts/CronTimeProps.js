7c1308ae9888c51c6a5eac38024475eae4aed27a
Lukas Hlavac <hlavac.l@hanaboso.com>
2017-11-07 14:24:36 +0100

    BPMN: cron param fix

--- src/views/elements/bpmn/custom-modeler/PropertiesPanel/parts/CronTimeProps.js
+++ src/views/elements/bpmn/custom-modeler/PropertiesPanel/parts/CronTimeProps.js
@@ -1,10 +1,9 @@
 import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
 import cmdHelper from 'bpmn-js-properties-panel/lib/helper/CmdHelper';
 import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
-import cronParser from 'cron-parser';
 
 export default function (group, element, translate) {
-  if (element.pipesType !== 'cron') {
+  if (element.pipesType !== 'cron' && (!element.businessObject || element.businessObject.pipesType !== 'cron')) {
     return;
   }
 
