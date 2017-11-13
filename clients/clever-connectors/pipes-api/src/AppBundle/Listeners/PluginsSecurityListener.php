859f3947c1dd9c809211307b239acb73bfed332b
Stanislav Kundrat <kundrat.s@hanaboso.com>
2017-11-13 16:46:21 +0100

    PluginsSecurityListener: added guid and token check for install action

--- clients/clever-connectors/pipes-api/src/AppBundle/Listeners/PluginsSecurityListener.php
+++ clients/clever-connectors/pipes-api/src/AppBundle/Listeners/PluginsSecurityListener.php
@@ -5,6 +5,10 @@ namespace CleverConnectors\AppBundle\Listeners;
 use CleverConnectors\AppBundle\Controller\PluginsController;
 use CleverConnectors\AppBundle\Exceptions\CleverConnectorsException;
 use CleverConnectors\AppBundle\Model\Plugins\PluginsSecurityManager;
+use Exception;
+use GuzzleHttp\Psr7\Uri;
+use Hanaboso\PipesFramework\Commons\Transport\Curl\Dto\RequestDto;
+use Hanaboso\PipesFramework\Commons\Transport\CurlManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -23,13 +27,20 @@ class PluginsSecurityListener implements EventSubscriberInterface
     private $security;
 
     /**
+     * @var CurlManagerInterface
+     */
+    private $curlManager;
+
+    /**
      * PluginSecurityListener constructor.
      *
      * @param PluginsSecurityManager $security
+     * @param CurlManagerInterface   $curlManager
      */
-    function __construct(PluginsSecurityManager $security)
+    function __construct(PluginsSecurityManager $security, CurlManagerInterface $curlManager)
     {
-        $this->security = $security;
+        $this->security    = $security;
+        $this->curlManager = $curlManager;
     }
 
     /**
@@ -44,9 +55,39 @@ class PluginsSecurityListener implements EventSubscriberInterface
             return;
         }
 
-        if ($inf[0] instanceof PluginsController && $inf[1] !== 'installAction') {
-            $headers = $ev->getRequest()->headers;
-            $this->security->checkSystemInstall($headers->all());
+        $eventReq = $ev->getRequest();
+
+        if ($inf[0] instanceof PluginsController) {
+            if ($inf[1] == 'installAction') {
+
+                $userId = $eventReq->attributes->get('userId');
+                $token  = $eventReq->attributes->get('token');
+
+                $req = new RequestDto('GET', new Uri('https://api.dev.clevermonitor.com/v1.2'));
+                $req->setHeaders([
+                    'Accept'    => 'application/json',
+                    'X-Api-Key' => sprintf('%s:%s', $userId, $token),
+                ]);
+
+                try {
+                    $req  = $this->curlManager->send($req);
+                    $code = $req->getStatusCode();
+                    $text = '';
+                } catch (Exception $e) {
+                    $code = 400;
+                    $text = $e->getMessage();
+                }
+
+                if ($code != 200) {
+                    throw new CleverConnectorsException(
+                        sprintf('User [%s] with token [%s] was not found. || ' . $text, $userId, $token),
+                        CleverConnectorsException::USER_TOKEN_NOT_EXISTS
+                    );
+                }
+
+            } else {
+                $this->security->checkSystemInstall($eventReq->headers->all());
+            }
         }
     }
 
