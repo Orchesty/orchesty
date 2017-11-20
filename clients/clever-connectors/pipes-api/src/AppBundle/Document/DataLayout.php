848d82a6379fbded0ee4d2a60cfb4248d8c44b3f
Stanislav Kundrat <kundrat.s@hanaboso.com>
2017-11-20 16:42:11 +0100

    DataLayout document, LayoutField object and DataLayoutActionEnum

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Document/DataLayout.php
@@ -0,0 +1,112 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Document;
+
+use CleverConnectors\AppBundle\Document\Traits\IdTrait;
+use CleverConnectors\AppBundle\Enum\DataLayoutActionEnum;
+use CleverConnectors\AppBundle\Model\DataLayout\LayoutField;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * Class DataLayout
+ *
+ * @package CleverConnectors\AppBundle\Document
+ *
+ * @ODM\Document(repositoryClass="CleverConnectors\AppBundle\Repository\DataLayoutRepository")
+ */
+class DataLayout
+{
+
+    use IdTrait;
+
+    /**
+     * @var string
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $action;
+
+    /**
+     * @var string
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $systemInstall;
+
+    /**
+     * @var LayoutField[]|ArrayCollection
+     *
+     * @ODM\Field(type="collection")
+     */
+    protected $fields;
+
+    /**
+     * DataLayout constructor.
+     */
+    public function __construct()
+    {
+        $this->fields = new ArrayCollection();
+    }
+
+    /**
+     * @param DataLayoutActionEnum $action
+     *
+     * @return DataLayout
+     */
+    public function setAction(DataLayoutActionEnum $action): DataLayout
+    {
+        $this->action = $action->getValue();
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param SystemInstall $systemInstall
+     *
+     * @return DataLayout
+     */
+    public function setSystemInstall(SystemInstall $systemInstall): DataLayout
+    {
+        $this->systemInstall = $systemInstall->getId();
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSystemInstall(): string
+    {
+        return $this->systemInstall;
+    }
+
+    /**
+     * @param LayoutField $field
+     *
+     * @return DataLayout
+     */
+    public function addField(LayoutField $field)
+    {
+        $this->fields[] = $field;
+
+        return $this;
+    }
+
+    /**
+     * @return LayoutField[]|ArrayCollection
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+}
