42b8921da38227986fade98fda1e1f0131cb2a86
Stanislav Kundrat <stanislav.kundrat@elnino.cz>
2017-12-14 22:43:32 +0100

    Shopify - distributing list wip

--- clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Shopify/ShopifySystem.php
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Shopify/ShopifySystem.php
@@ -215,12 +215,20 @@ class ShopifySystem implements WebhookSystemInterface, OAuth2Interface, CMEventS
             $systemInstall->isEventHardBounce()
         );
 
+        $field5 = new Field(
+            Field::SELECT,
+            SystemInstall::SELECT_LIST,
+            'Distribution list',
+            $this->prepareValue(SystemInstall::SELECT_LIST, $settings)
+        );
+
         $form = new Form();
         $form
             ->addField($field1)
             ->addField($field2)
             ->addField($field3)
-            ->addField($field4);
+            ->addField($field4)
+            ->addField($field5);
 
         return $form->toArray();
     }
