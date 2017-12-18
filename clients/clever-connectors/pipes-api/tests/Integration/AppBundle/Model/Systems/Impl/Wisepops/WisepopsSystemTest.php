3aefd55236e90562a0ed14f3501af51123c7dbb1
Radek Bruha <bruha.r@hanaboso.com>
2017-12-18 10:00:56 +0100

    WisePops System: Add saveCustomForm test

---
+++ clients/clever-connectors/pipes-api/tests/Integration/AppBundle/Model/Systems/Impl/Wisepops/WisepopsSystemTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration\AppBundle\Model\Systems\Impl\Wisepops;
+
+use CleverConnectors\AppBundle\Document\SystemInstall;
+use Tests\DatabaseTestCaseAbstract;
+
+/**
+ * Class WisepopsSystemTest
+ *
+ * @package Tests\Integration\AppBundle\Model\Systems\Impl\Wisepops
+ */
+final class WisepopsSystemTest extends DatabaseTestCaseAbstract
+{
+
+    /**
+     *
+     */
+    public function testRunSaveCustomForm(): void
+    {
+        $systemInstall = (new SystemInstall())
+            ->setSystem('wisepops')
+            ->setUser('User')
+            ->setToken('Token');
+        $this->persistAndFlush($systemInstall);
+
+        $manager = $this->container->get('cc.systems.manager');
+        $manager->runCustomAction('wisepops', 'User', 'saveCustomForm', ['key' => 'value']);
+
+        $this->dm->clear();
+        $systemInstall = $this->dm->getRepository(SystemInstall::class)->find($systemInstall->getId());
+
+        $this->assertEquals([
+            'custom_form' => [
+                'key' => 'value',
+            ],
+        ], $systemInstall->getSettings());
+    }
+
+}
