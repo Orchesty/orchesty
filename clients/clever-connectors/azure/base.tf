f0cace775f99623a4b20ae09f5c7ab4c7b996a5f
Jakub Husák <husak.j@gmail.com>
2017-12-19 17:21:04 +0100

    CM Azure infra: fix ssh port in security group

--- clients/clever-connectors/azure/base.tf
+++ clients/clever-connectors/azure/base.tf
@@ -53,7 +53,7 @@ resource "azurerm_network_security_group" "default" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "21"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
