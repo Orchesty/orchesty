33fec871149dcff9371c87d6f1b68fbae51bb5b2
Jakub Husák <husak.j@gmail.com>
2017-12-19 11:22:45 +0100

    CM Azure stack: added swarm nodes 03 and 04

--- clients/clever-connectors/azure/swarm01.tf
+++ clients/clever-connectors/azure/swarm01.tf
@@ -18,3 +18,21 @@ module "swarm01n02" {
   subnet_id           = "${azurerm_subnet.swarm01.id}"
   ssh_keys            = "${var.common_ssh_keys}"
 }
+
+module "swarm01n03" {
+  source              = "./modules/swarm_node"
+  name                = "swarm01n03"
+  location            = "${azurerm_resource_group.main.location}" 
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  subnet_id           = "${azurerm_subnet.swarm01.id}"
+  ssh_keys            = "${var.common_ssh_keys}"
+}
+
+module "swarm01n04" {
+  source              = "./modules/swarm_node"
+  name                = "swarm01n04"
+  location            = "${azurerm_resource_group.main.location}" 
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  subnet_id           = "${azurerm_subnet.swarm01.id}"
+  ssh_keys            = "${var.common_ssh_keys}"
+}
