fdf225d3c1c0feb7e0cb7773630809ebad8342ab
David Horacek <horacek.d@hanaboso.com>
2018-01-02 14:25:01 +0100

    Adds check for id & secret

--- clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Salesforce/SalesforceSystem.php
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Salesforce/SalesforceSystem.php
@@ -271,8 +271,15 @@ class SalesforceSystem implements OAuth2Interface, CMEventSystemInterface
      */
     private function createDto(SystemInstall $systemInstall): OAuth2Dto
     {
-        $redirectUrl = AuthorizationUtils::generateUrl();
         $sett = $systemInstall->getSettings();
+        if (!array_key_exists(self::CLIENT_ID, $sett)
+            || !array_key_exists(self::CLIENT_SECRET, $sett)) {
+            throw new SystemException::MISSING_DATA(
+                'Missing Client Id or Client secret in settings.'
+            );
+        }
+
+        $redirectUrl = AuthorizationUtils::generateUrl();
 
         $dto = new OAuth2Dto($sett[self::CLIENT_ID], $sett[self::CLIENT_SECRET], $redirectUrl, self::AUTHORIZE_URL, self::TOKEN_URL);
         $dto->setCustomAppDependencies($systemInstall->getUser(), $systemInstall->getSystem());
