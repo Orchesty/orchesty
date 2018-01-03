ab66deecab3efb113068051f80d37bf3f97e37fe
David Horacek <horacek.d@hanaboso.com>
2018-01-03 16:31:12 +0100

    Check for keys in splitter

--- clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Airtable/Splitter/AirtableUpdateTableSplitter.php
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/Systems/Impl/Airtable/Splitter/AirtableUpdateTableSplitter.php
@@ -4,6 +4,7 @@ namespace CleverConnectors\AppBundle\Model\Systems\Impl\Airtable\Splitter;
 
 use CleverConnectors\AppBundle\Document\SystemInstall;
 use CleverConnectors\AppBundle\Enum\CleverFieldsEnum;
+use CleverConnectors\AppBundle\Exceptions\CleverConnectorsException;
 use CleverConnectors\AppBundle\Model\Systems\Impl\Airtable\AirtableSystem;
 use CleverConnectors\AppBundle\Utils\CMHeaders;
 use Hanaboso\PipesFramework\Commons\Process\ProcessDto;
@@ -25,14 +26,28 @@ class AirtableUpdateTableSplitter extends AirtableTableSplitter
      * @param callable      $callbackItem
      *
      * @return PromiseInterface
+     * @throws CleverConnectorsException
      */
     public function processBatch(ProcessDto $dto, LoopInterface $loop, callable $callbackItem): PromiseInterface
     {
         $sys  = $this->systemInstallRepository->getSystemInstallFromHeaders($dto->getHeaders());
         $sett = $sys->getSettings();
 
-        $data  = json_decode($dto->getData(), TRUE);
+        $data = json_decode($dto->getData(), TRUE);
+
+        if (!array_key_exists(CleverFieldsEnum::LISTS, $data)) {
+            throw new CleverConnectorsException(
+                'Missing required field [lists] in data - required list of distribution groups.',
+                CleverConnectorsException::MISSING_DATA
+            );
+        }
         $lists = $data[CleverFieldsEnum::LISTS];
+        if (!is_array($lists)) {
+            throw new CleverConnectorsException(
+                'Field [lists] is not an array - required list of distribution groups.',
+                CleverConnectorsException::MISSING_DATA
+            );
+        }
 
         $i = 0;
         if (array_key_exists(SystemInstall::FORMS, $sett)) {
@@ -42,6 +57,13 @@ class AirtableUpdateTableSplitter extends AirtableTableSplitter
                     $callbackItem($this->createSuccessMessage($form, $i, $dto));
                 }
             }
+        }
+
+        if ($i == 0) {
+            throw new CleverConnectorsException(
+                'None of given lists has been found in settings.',
+                CleverConnectorsException::REQUEST_FAILED
+            );
         }
 
         $processId = CMHeaders::get(CMHeaders::PROCESS_ID, $dto->getHeaders()) ?? '';
