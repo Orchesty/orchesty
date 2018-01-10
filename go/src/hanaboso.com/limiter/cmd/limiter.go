93254744d1edf5b8c72fb9e83ffc6b78c4be6a88
Tomas Sedlacek <mail@kedlas.cz>
2018-01-10 16:18:13 +0100

    go: limiter tcp server prototype

--- go/src/hanaboso.com/limiter/cmd/limiter.go
+++ go/src/hanaboso.com/limiter/cmd/limiter.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"hanaboso.com/limiter/pkg/server"
+	"os"
+	"os/signal"
+	"syscall"
+	"log"
 )
 
-// main runs the
+// main runs the limiter program
 func main() {
-	fmt.Println("I am limiter.")
+	tcpServer := server.TcpServer{}
+	go tcpServer.Start()
+
+	gracefulShutdown(&tcpServer)
 }
+
+// gracefulShutdown handles SIGINT and SIGTERM signal to stop the app gracefully
+func gracefulShutdown(tcpServer *server.TcpServer) {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		log.Println()
+		log.Println("Signal received: ", sig)
+
+		// tcpServer.Stop()
+
+		done <- true
+	}()
+
+	<-done
+}
+
+
