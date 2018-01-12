7dcf738c95ca61df19954532ef45769059b6af6b
David Horacek <horacek.d@hanaboso.com>
2018-01-12 12:40:55 +0100

    Pipedrive tests for limits

--- clients/clever-connectors/pipes-api/tests/Unit/AppBundle/Model/Systems/Impl/Pipedrive/Connector/PipedriveCreatePersonConnectorTest.php
+++ clients/clever-connectors/pipes-api/tests/Unit/AppBundle/Model/Systems/Impl/Pipedrive/Connector/PipedriveCreatePersonConnectorTest.php
@@ -7,12 +7,16 @@ use CleverConnectors\AppBundle\Enum\CleverCustomKeysEnum;
 use CleverConnectors\AppBundle\Model\Systems\Impl\Pipedrive\Connector\PipedriveCreatePersonConnector;
 use CleverConnectors\AppBundle\Repository\SystemInstallRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Uri;
 use Hanaboso\PipesFramework\Commons\Process\ProcessDto;
+use Hanaboso\PipesFramework\Commons\Transport\Curl\CurlException;
 use Hanaboso\PipesFramework\Commons\Transport\Curl\Dto\RequestDto;
 use Hanaboso\PipesFramework\Commons\Transport\Curl\Dto\ResponseDto;
 use Hanaboso\PipesFramework\Commons\Transport\CurlManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit_Framework_MockObject_MockObject;
+use Psr\Log\LoggerInterface;
 use Tests\KernelTestCaseAbstract;
 
 /**
@@ -49,14 +53,59 @@ final class PipedriveCreatePersonConnectorTest extends KernelTestCaseAbstract
     }
 
     /**
-     * @return CurlManagerInterface|MockObject
+     *
      */
-    private function mockCurl(): CurlManagerInterface
+    public function testConnectorLimit(): void
     {
-        $test = $this;
+        $conn = new PipedriveCreatePersonConnector(
+            $this->container->get('systems.pipedrive'),
+            $this->mockCurl(429),
+            $this->mockDm()
+        );
+
+        $dto = new ProcessDto();
+        $dto->setHeaders([])->setData('');
+
+        $res = $conn->processAction($dto);
+
+        self::assertArrayHasKey('pf-result-code', $res->getHeaders());
+        self::assertEquals(1004, $res->getHeaders()['pf-result-code']);
+    }
+
+    /**
+     *
+     */
+    public function testConnectorError(): void
+    {
+        $conn = new PipedriveCreatePersonConnector(
+            $this->container->get('systems.pipedrive'),
+            $this->mockCurl(404),
+            $this->mockDm()
+        );
+
+        $dto = new ProcessDto();
+        $dto->setHeaders([])->setData('');
+
+        $conn->setLogger($this->mockLogger());
+        $this->expectException(CurlException::class);
+        $conn->processAction($dto);
+    }
+
+    /**
+     * @param int $status
+     *
+     * @return CurlManagerInterface
+     */
+    private function mockCurl(int $status = 201): CurlManagerInterface
+    {
+        /** @var CurlManagerInterface|PHPUnit_Framework_MockObject_MockObject $curl */
         $curl = $this->createMock(CurlManagerInterface::class);
         $curl->expects($this->at(0))
-            ->method('send')->will($this->returnCallback(function (RequestDto $dto) use ($test) {
+            ->method('send')->will($this->returnCallback(function (RequestDto $dto) use ($status) {
+                if ($status >= 300) {
+                    throw new CurlException('', 0, NULL, new Response($status));
+                }
+
                 $expt = new RequestDto('POST', new Uri('https://api.pipedrive.com/v1/persons?api_token=token'));
                 $expt->setHeaders([
                     'Accept'       => 'application/json',
@@ -69,9 +118,9 @@ final class PipedriveCreatePersonConnectorTest extends KernelTestCaseAbstract
                     ]),
                 ]));
 
-                $test->assertEquals($expt, $dto);
+                self::assertEquals($expt, $dto);
 
-                return new ResponseDto(201, '', '', []);
+                return new ResponseDto($status, '', '', []);
             }));
 
         return $curl;
@@ -86,7 +135,7 @@ final class PipedriveCreatePersonConnectorTest extends KernelTestCaseAbstract
         $sys->setSettings([
             CleverCustomKeysEnum::HARD_BOUNCE => 'hardhash',
             'api_token'                       => 'token',
-        ]);
+        ])->setUser('usr')->setToken('tkn');
 
         $repo = $this->createMock(SystemInstallRepository::class);
         $repo->expects($this->once())
@@ -97,6 +146,29 @@ final class PipedriveCreatePersonConnectorTest extends KernelTestCaseAbstract
             ->method('getRepository')->willReturn($repo);
 
         return $dm;
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    private function mockLogger(): LoggerInterface
+    {
+        /** @var LoggerInterface|PHPUnit_Framework_MockObject_MockObject $logger */
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method('info')->will($this->returnCallback(
+                function (string $type, $data): void {
+                    self::assertEquals('data_error', $type);
+                    self::assertEquals([
+                        'guid'        => 'usr',
+                        'token'       => 'tkn',
+                        'system_key'  => 'pipedrive',
+                        'system_name' => 'Pipedrive',
+                    ], $data);
+                }
+            ));
+
+        return $logger;
     }
 
 }
