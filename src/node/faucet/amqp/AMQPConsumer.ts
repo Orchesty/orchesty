28d9e90e5756f37896adefe834212fa58c9f1c3f
Tomas Sedlacek <mail@kedlas.cz>
2018-01-19 11:29:48 +0100

    bridge: accept pf-published-timestamp header

--- src/node/faucet/amqp/AMQPConsumer.ts
+++ src/node/faucet/amqp/AMQPConsumer.ts
@@ -2,11 +2,28 @@ import { Channel, Message } from "amqplib";
 import {Connection} from "amqplib-plus/dist/lib/Connection";
 import { Consumer as BasicConsumer } from "amqplib-plus/dist/lib/Consumer";
 import logger from "../../../logger/Logger";
+import Headers from "../../../message/Headers";
 import JobMessage from "../../../message/JobMessage";
 import {INodeLabel} from "../../../topology/Configurator";
 import {FaucetProcessMsgFn} from "../IFaucet";
 
 class Consumer extends BasicConsumer {
+
+    /**
+     * Returns the message published timestamp [ms]
+     *
+     * @param {JobMessage}jobMsg
+     * @param {Message} amqMsg
+     * @returns {number}
+     */
+    private static getPublishedTimestamp(jobMsg: JobMessage, amqMsg: Message): number {
+        let published = amqMsg.properties.timestamp;
+        if (jobMsg.getHeaders().hasPFHeader(Headers.PUBLISHED_TIMESTAMP)) {
+            published = jobMsg.getHeaders().getPFHeader(Headers.PUBLISHED_TIMESTAMP);
+        }
+
+        return parseInt(published, 10);
+    }
 
     private node: INodeLabel;
     private processData: FaucetProcessMsgFn;
@@ -27,7 +44,7 @@ class Consumer extends BasicConsumer {
         try {
             inMsg = new JobMessage(this.node, amqMsg.properties.headers, amqMsg.content);
             inMsg.getMeasurement().markReceived();
-            inMsg.getMeasurement().setPublished(parseInt(amqMsg.properties.timestamp, 10));
+            inMsg.getMeasurement().setPublished(Consumer.getPublishedTimestamp(inMsg, amqMsg));
             inMsg.getHeaders().setHeader("content-type", amqMsg.properties.contentType);
 
             logger.info(`AmqpFaucet received message.`, logger.ctxFromMsg(inMsg));
