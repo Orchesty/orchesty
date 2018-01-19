7e0fb152fd9c9faafe1687533e05d46a20897acb
Radek Jirsa <jirsa.r@hanaboso.com>
2018-01-19 08:48:16 +0100

    Composer updated & MetricsManager: updated parsing of resultSet

--- pf-bundles/src/Metrics/Exception/MetricsException.php
+++ pf-bundles/src/Metrics/Exception/MetricsException.php
@@ -24,5 +24,6 @@ final class MetricsException extends Exception
     public const DB_NOT_EXIST       = self::OFFSET + 1;
     public const NODE_NOT_FOUND     = self::OFFSET + 2;
     public const TOPOLOGY_NOT_FOUND = self::OFFSET + 3;
+    public const QUERY_ERROR        = self::OFFSET + 4;
 
 }
