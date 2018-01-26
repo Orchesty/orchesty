eddf25d492a229088c02cbfb901ee293fdc29ea1
David Horacek <horacek.d@hanaboso.com>
2018-01-26 15:01:39 +0100

    Rabbit custom node for message spam

---
+++ src/CustomNode/Model/Batch/BatchProducer.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\CustomNode\Model\Batch;
+
+use Hanaboso\PipesFramework\RabbitMq\Producer\AbstractProducer;
+
+/**
+ * Class BatchProducer
+ *
+ * @package Hanaboso\PipesFramework\CustomNode\Model\Batch
+ */
+class BatchProducer extends AbstractProducer
+{
+
+}
