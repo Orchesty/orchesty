caf611c8ca1a7ded11f1bb53d9b11c4d0e59c58d
David Horacek <horacek.d@hanaboso.com>
2018-02-01 16:31:32 +0100

    Test topology

--- clients/clever-connectors/pipes-api/src/AppBundle/Model/CM/TestNotificationConnector/CMTestNotificationConnector.php
+++ clients/clever-connectors/pipes-api/src/AppBundle/Model/CM/TestNotificationConnector/CMTestNotificationConnector.php
@@ -3,12 +3,7 @@
 namespace CleverConnectors\AppBundle\Model\CM\TestNotificationConnector;
 
 use CleverConnectors\AppBundle\Document\SystemInstall;
-use CleverConnectors\AppBundle\Model\Systems\SystemInterface;
-use CleverConnectors\AppBundle\Repository\SystemInstallRepository;
 use CleverConnectors\AppBundle\Traits\LoggerTrait;
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\ODM\MongoDB\DocumentManager;
-use Hanaboso\PipesFramework\Authorization\Provider\OAuth2Provider;
 use Hanaboso\PipesFramework\Commons\Process\ProcessDto;
 use Hanaboso\PipesFramework\CustomNode\CustomNodeInterface;
 use Psr\Log\LoggerAwareInterface;
@@ -25,41 +20,23 @@ class CMTestNotificationConnector implements CustomNodeInterface, LoggerAwareInt
     use LoggerTrait;
 
     /**
-     * @var SystemInterface
-     */
-    private $system;
-
-    /**
-     * @var SystemInstallRepository|ObjectRepository
-     */
-    private $systemInstallRepository;
-
-    /**
-     * CMTestNotificationConnector constructor.
-     *
-     * @param DocumentManager $dm
-     * @param OAuth2Provider  $provider
-     */
-    public function __construct(DocumentManager $dm, OAuth2Provider $provider)
-    {
-        $this->systemInstallRepository = $dm->getRepository(SystemInstall::class);
-        $this->system                  = new NullSystem($provider);
-    }
-
-    /**
      * @param ProcessDto $dto
      *
      * @return ProcessDto
      */
     public function process(ProcessDto $dto): ProcessDto
     {
-        $systemInstall = $this->systemInstallRepository->getSystemInstallFromHeaders($dto->getHeaders());
-        $data          = $dto->getData() ? json_decode($dto->getData(), TRUE) : NULL;
+        $data = $dto->getData() ? json_decode($dto->getData(), TRUE) : NULL;
 
         if ($data) {
+            $systemInstall = new SystemInstall();
+            $systemInstall->setUser('user-guid')
+                ->setToken('token');
+
             foreach ($data as $key => $value) {
                 if ($value) {
-                    $this->logger->info($key, self::getMessage($key, $this->system, $systemInstall));
+
+                    $this->logger->info($key, self::getMessage($key, new NullSystem(), $systemInstall));
                 }
             }
         }
