0119962e9c778464f599f231b37a3b6012b2fdfc
Tomas Sedlacek <mail@kedlas.cz>
2018-02-06 10:59:07 +0100

    php performance: fixed performance test

--- clients/clever-connectors/pipes-api/web/app.php
+++ clients/clever-connectors/pipes-api/web/app.php
@@ -2,7 +2,19 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-if (getallheaders()["pf-force-plain-php-response"] === "true") {
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+    }
+}
+
+if (getallheaders()["Pf-Force-Plain-Php-Response"] === "true") {
     header('Content-Type: application/json');
     echo json_encode(["status" => "success"]);
     return;
