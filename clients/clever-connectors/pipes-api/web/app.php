60bfe95ad5e0af3f0d42f1f0865e99ce96a2547a
Tomas Sedlacek <mail@kedlas.cz>
2018-02-07 13:46:46 +0100

    performance test: removed performance test code

--- clients/clever-connectors/pipes-api/web/app.php
+++ clients/clever-connectors/pipes-api/web/app.php
@@ -2,24 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-if (!function_exists('getallheaders')) {
-    function getallheaders() {
-    $headers = [];
-    foreach ($_SERVER as $name => $value) {
-        if (substr($name, 0, 5) == 'HTTP_') {
-            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-        }
-    }
-    return $headers;
-    }
-}
-
-if (getallheaders()["Pf-Force-Plain-Php-Response"] === "true") {
-    header('Content-Type: application/json');
-    echo json_encode(["status" => "success"]);
-    return;
-}
-
 require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
