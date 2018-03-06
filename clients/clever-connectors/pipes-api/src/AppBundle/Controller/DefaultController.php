5df77113ba3073feeef4f218d59200c4fbac4bde
Radek Jirsa <jirsa.r@hanaboso.com>
2018-03-06 10:41:14 +0100

    Added default controller

---
+++ clients/clever-connectors/pipes-api/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace CleverConnectors\AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Hanaboso\PipesFramework\Commons\Traits\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class DefaultController
+ *
+ * @package CleverConnectors\AppBundle\Controller
+ *
+ * @Route(service="cc.default.controller")
+ */
+class DefaultController extends FOSRestController
+{
+
+    use ControllerTrait;
+
+    /**
+     * @Route("/")
+     * @Method("GET")
+     *
+     * @return Response
+     */
+    public function defaultAction(): Response
+    {
+        return $this->getResponse('OK', 200);
+    }
+
+}
