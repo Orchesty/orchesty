fdec480a4b2e86a131dc544f9d7d68bb10bfc5ce
David Horacek <horacek.d@hanaboso.com>
2018-03-14 10:07:46 +0100

    Debug to requestbin

---
+++ pf-bundles/src/CustomNode/Impl/RequestbinConnector.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\CustomNode\Impl;
+
+use GuzzleHttp\Psr7\Uri;
+use Hanaboso\PipesFramework\Commons\Process\ProcessDto;
+use Hanaboso\PipesFramework\Commons\Transport\Curl\CurlManager;
+use Hanaboso\PipesFramework\Commons\Transport\Curl\Dto\RequestDto;
+use Hanaboso\PipesFramework\CustomNode\CustomNodeInterface;
+
+/**
+ * Class RequestbinConnector
+ *
+ * @package Hanaboso\PipesFramework\CustomNode\Impl
+ */
+class RequestbinConnector implements CustomNodeInterface
+{
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var CurlManager
+     */
+    private $curl;
+
+    /**
+     * RequestbinConnector constructor.
+     *
+     * @param string      $url
+     * @param CurlManager $curl
+     */
+    public function __construct(string $url, CurlManager $curl)
+    {
+        $this->url  = $url;
+        $this->curl = $curl;
+    }
+
+    /**
+     * @param ProcessDto $dto
+     *
+     * @return ProcessDto
+     */
+    public function process(ProcessDto $dto): ProcessDto
+    {
+        $req = new RequestDto(CurlManager::METHOD_POST, new Uri($this->url));
+        $req->setBody($dto->getData())
+            ->setHeaders($dto->getHeaders());
+
+        $this->curl->send($req);
+
+        return $dto;
+    }
+
+}
