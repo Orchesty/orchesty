6d6ce9a20332f18a6e2fba603d7b848fd6c8be93
Marcel Pavlicek <pavlicek.m@hanaboso.com>
2018-03-26 09:40:41 +0200

    Typo

--- kapacitor/entrypoint.sh
+++ kapacitor/entrypoint.sh
@@ -3,8 +3,8 @@
 DB=pipes
 
 curl --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "5s" ON '$DB' DURATION 168h REPLICATION 1'
-url --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "1m" ON '$DB' DURATION 168h REPLICATION 1'
-url --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "30m" ON '$DB' DURATION 168h REPLICATION 1'
-url --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "4h" ON '$DB' DURATION 168h REPLICATION 1'
+curl --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "1m" ON '$DB' DURATION 168h REPLICATION 1'
+curl --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "30m" ON '$DB' DURATION 168h REPLICATION 1'
+curl --retry 5 -XPOST 'http://influxdb:8086/query?db=pipes' --data-urlencode 'q=CREATE RETENTION POLICY "4h" ON '$DB' DURATION 168h REPLICATION 1'
 
 exec /entrypoint.sh "$@"
