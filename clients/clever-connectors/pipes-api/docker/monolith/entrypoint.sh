c1af09b8d32dd0e044e7f9fb6267c93c279393e6
Jakub Husák <husak.j@gmail.com>
2018-03-26 16:16:12 +0200

    clever pipes-api: fix ngnix-in-monolith subprocess issues

--- clients/clever-connectors/pipes-api/docker/monolith/entrypoint.sh
+++ clients/clever-connectors/pipes-api/docker/monolith/entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 
 if [ "$1" == "php-fpm" ]; then
 	# Run nginx with a slight delay to avoid serving 504 until PHP starts
-	# TODO: use some lightweight process control system
-	sh -c 'sleep 2; nginx' &
+	# TODO: maybe use some lightweight process control system
+	sh -c 'sleep 3; nginx' &
 fi
 
-exec "$@"
+# No typical "exec" here to avoid breaking our delayed nginx start
+"$@"
