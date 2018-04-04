c4dc08a2c63771591c1ed915e94368e2c4c88405
Tomas Sedlacek <mail@kedlas.cz>
2018-04-04 11:49:47 +0200

    bridge: debug worker logging

--- pf-bridge/src/node/worker/NullWorker.ts
+++ pf-bridge/src/node/worker/NullWorker.ts
@@ -29,22 +29,32 @@ class NullWorker extends AWorker {
      * @inheritdoc
      */
     public processData(msg: JobMessage): Promise<JobMessage[]> {
-        logger.debug(
-            `Worker[type="null"] processing message.`,
-            {
-                correlation_id: msg.getCorrelationId(),
-                node_id: this.settings.node_label.node_id,
-                node_name: this.settings.node_label.node_name,
-                topology_id: this.settings.node_label.topology_id,
-                data : JSON.stringify(msg.getHeaders().getRaw()),
-            },
-        );
-
-        msg.setResult({code: ResultCode.SUCCESS, message: "Null worker passed message."});
-
         if (this.settings && this.settings.node_label.node_name.toLowerCase() === "debug") {
-            // Add some custom logic here for ad-hoc testing
             msg.setResult({code: ResultCode.SUCCESS, message: "Debug worker passed message."});
+
+            logger.info(
+                `Worker[type="debug"] processing message.`,
+                {
+                    correlation_id: msg.getCorrelationId(),
+                    node_id: this.settings.node_label.node_id,
+                    node_name: this.settings.node_label.node_name,
+                    topology_id: this.settings.node_label.topology_id,
+                    data : JSON.stringify({headers: msg.getHeaders().getRaw(), data: msg.getContent()}),
+                },
+            );
+        } else {
+            msg.setResult({code: ResultCode.SUCCESS, message: "Null worker passed message."});
+
+            logger.debug(
+                `Worker[type="null"] processing message.`,
+                {
+                    correlation_id: msg.getCorrelationId(),
+                    node_id: this.settings.node_label.node_id,
+                    node_name: this.settings.node_label.node_name,
+                    topology_id: this.settings.node_label.topology_id,
+                    data : JSON.stringify(msg.getHeaders().getRaw()),
+                },
+            );
         }
 
         return Promise.resolve([msg]);
