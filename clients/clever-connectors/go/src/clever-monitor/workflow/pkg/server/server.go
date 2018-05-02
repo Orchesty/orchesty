75b8c1fc6db3f32065f3ddbc2fcff5ae7355c6a3
Tomas Sedlacek <mail@kedlas.cz>
2018-05-02 09:24:32 +0200

    Workflow: logging grpc method calls using interceptor

--- clients/clever-connectors/go/src/clever-monitor/workflow/pkg/server/server.go
+++ clients/clever-connectors/go/src/clever-monitor/workflow/pkg/server/server.go
@@ -10,9 +10,8 @@ import (
 	"clever-monitor/utils/logger"
 	"clever-monitor/workflow/pkg/handler"
 	ws "clever-monitor/workflow/pkg/workflowservice"
+	"time"
 )
-
-const MaxInt = int(^uint(0) >> 1)
 
 type server struct {
 	addr           string
@@ -40,7 +39,7 @@ func (s *server) Start() {
 
 	s.logger.Info(fmt.Sprintf("Grpc Tcp server running on: %s", s.addr), logger.Context{})
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(s.loggingInterceptor))
 
 	s.logger.Info("Grpc server registering WorkflowService", logger.Context{})
 	ws.RegisterWorkflowServiceServer(grpcServer, s)
@@ -53,99 +52,45 @@ func (s *server) Start() {
 	}
 }
 
-
-// TODO - implement logging using middleware
-// https://github.com/grpc-ecosystem/go-grpc-middleware/tree/master/logging/zap
-// https://blog.gopheracademy.com/advent-2017/go-grpc-beyond-basics/
-
 // CreateWorkflow creates new workflow
 func (s *server) CreateWorkflow(ctx context.Context, in *ws.CreateRequest) (*ws.WorkflowResponse, error) {
-	reqId := s.getRequestId("create_workflow")
-	go s.logRequest(reqId)
-
-	response := s.wfHandler.HandleCreate(in)
-
-	go s.logResponse(response, reqId)
-
-	return response, nil
+	return s.wfHandler.HandleCreate(in), nil
 }
 
 // DeleteWorkflow removes existing workflow
 func (s *server) DeleteWorkflow(ctx context.Context, in *ws.DeleteRequest) (*ws.WorkflowResponse, error) {
-	reqId := s.getRequestId("delete_workflow")
-	go s.logRequest(reqId)
-
-	response := s.wfHandler.HandleDelete(in)
-
-	go s.logResponse(response, reqId)
-
-	return response, nil
+	return s.wfHandler.HandleDelete(in), nil
 }
 
 // ReadEditorConfig returns editor config passed during create/update workflow
 func (s *server) ReadEditorConfig(ctx context.Context, in *ws.ReadRequest) (*ws.WorkflowResponse, error) {
-	reqId := s.getRequestId("read_config")
-	go s.logRequest(reqId)
-
-	response := s.wfHandler.HandleReadEditorConfig(in)
-
-	go s.logResponse(response, reqId)
-
-	return response, nil
+	return s.wfHandler.HandleReadEditorConfig(in), nil
 }
 
 // ReadWorkflowConfig returns content of existing generated workflow config
 func (s *server) ReadWorkflowConfig(ctx context.Context, in *ws.ReadRequest) (*ws.WorkflowResponse, error) {
-	reqId := s.getRequestId("read_workflow")
-	go s.logRequest(reqId)
-
-	response := s.wfHandler.HandleReadWorkflowConfig(in)
-
-	go s.logResponse(response, reqId)
-
-	return response, nil
+	return s.wfHandler.HandleReadWorkflowConfig(in), nil
 }
 
 // ReadAllWorkflowConfigs returns all generated workflow configs related to single editor config
 func (s *server) ReadAllWorkflowConfigs(ctx context.Context, in *ws.ReadAllRequest) (*ws.WorkflowResponse, error) {
-	reqId := s.getRequestId("read_workflow")
-	go s.logRequest(reqId)
-
-	response := s.wfHandler.HandleReadAllWorkflowConfigs(in)
-
-	go s.logResponse(response, reqId)
-
-	return response, nil
+	return s.wfHandler.HandleReadAllWorkflowConfigs(in), nil
 }
 
-// getRequestId returns id to be used to pair request and response
-func (s *server) getRequestId(method string) string {
-	if s.requestCount > MaxInt {
-		s.requestCount = 0
-	}
+// logging middleware function
+func (s *server) loggingInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	start := time.Now()
 
-	s.requestCount++
+	h, err := handler(ctx, req)
 
-	return fmt.Sprintf("#%d-%s", s.requestCount, method)
-}
+	msg := fmt.Sprintf("requested:%s , duration:%s", info.FullMethod, time.Since(start))
 
-// logRequest logs request using logger
-func (s *server) logRequest(reqId string) {
-	s.logger.Info("Request received", logger.Context{"reqId": reqId})
-}
+	s.logger.Info(msg, logger.Context{"error": err})
 
-// logResponse logs response using logger
-func (s *server) logResponse(response *ws.WorkflowResponse, reqId string) {
-	s.logger.Info(
-		fmt.Sprintf("Sending response. Code: '%d' Message: '%s'", response.Code, response.Message),
-		logger.Context{"reqId": reqId},
-	)
-}
-
-// logConfig logs WorkflowConfig response using logger
-func (s *server) logConfig(config *ws.WorkflowConfig, reqId string) {
-	s.logger.Info(
-		fmt.Sprintf("Sending config response. Id: '%s'", config.Id),
-		logger.Context{"reqId": reqId},
-	)
+	return h, err
 }
