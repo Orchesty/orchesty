4051cce8c2a1865bdf3ed7b3f5b105b74848c7a9
Tomas Sedlacek <mail@kedlas.cz>
2018-05-16 12:05:20 +0200

    cc workflow service moved to separate repository in cm gitlab. See it at: https://gitlab.clevermonitor.com/hanaboso/workflow

--- clients/clever-connectors/go/src/clever-monitor/workflow/pkg/generator/generator.go
+++
@@ -1,8 +0,0 @@
-package generator
-
-import ws "clever-monitor/workflow/pkg/workflowservice/clevermonitor/analytics/protos/workflow"
-
-type Generator interface {
-	// Generate returns the slice of generated workflow config due to given editor config data
-	Generate(editor *ws.EditorConfig, clientId int, guid string) ([]*ws.WorkflowConfig, error)
-}
