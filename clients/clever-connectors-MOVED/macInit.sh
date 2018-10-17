40885381851f778660bc30bb21cf5da65e5424bd
David Horacek <horacek.d@hanaboso.com>
2018-10-17 13:33:21 +0200

    Removed Clever files

--- clients/clever-connectors-MOVED/macInit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo ifconfig lo0 alias 127.0.0.67 up
-
-keypath="/Users/$USER/.docker/machine/machines/docker-pa/id_rsa"
-sshconfigpath=$(pwd)"/mac_ssh_config"
-
-sudo ssh -i $keypath -F $sshconfigpath mac_dsync-dev
