bee85f42c93bdc4fc5f36af4729957ac5de88f93
Radek Jirsa <jirsa.r@hanaboso.com>
2018-12-11 13:12:53 +0100

    Frontend: updated api conf

--- frontend/src/config/dev/servers.js
+++ frontend/src/config/dev/servers.js
@@ -1,21 +1,21 @@
 export default {
-  apiGateway: {
-    initDefault: 'cm_dev_docker',
-    servers: {
-      apiary: {
-        url: 'http://private-973c6-pipes1.apiary-mock.com',
-        noCredentials: true,
-        caption: 'Apiary',
-      },
-      cm_dev_docker: {
-        url: 'http://127.0.0.67/api',
-        caption: 'CM - Dev - docker',
-      },
-	    demo_dev_docker: {
-		    url: 'http://127.0.0.66/api',
-        url_starting_point: 'http://127.0.0.66/starting-point',
-		    caption: 'Demo - Dev - docker',
-	    },
-    },
-  },
+	apiGateway: {
+		initDefault: 'cm_dev_docker',
+		servers: {
+			apiary: {
+				url: 'http://private-973c6-pipes1.apiary-mock.com',
+				noCredentials: true,
+				caption: 'Apiary',
+			},
+			cm_dev_docker: {
+				url: 'http://127.0.0.67/api',
+				caption: 'CM - Dev - docker',
+			},
+			demo_dev_docker: {
+				url: 'http://127.0.0.66/api',
+				url_starting_point: 'http://127.0.0.66/starting-point',
+				caption: 'Demo - Dev - docker',
+			},
+		},
+	},
 };
