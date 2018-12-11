533ab9288abce8e5b143ce67516d96e663df52d4
Radek Jirsa <jirsa.r@hanaboso.com>
2018-12-11 13:12:53 +0100

    Frontend: updated api conf

--- src/config/prod/servers.js
+++ src/config/prod/servers.js
@@ -1,11 +1,12 @@
 export default {
-  apiGateway: {
-    initDefault: 'mainServer',
-    servers: {
-      mainServer: {
-        url: 'http://url.to.api.gateway',
-        caption: 'Main',
-      },
-    },
-  },
+	apiGateway: {
+		initDefault: 'mainServer',
+		servers: {
+			mainServer: {
+				url: 'http://url.to.api.gateway/api',
+				url_starting_point: 'http://url.to.api.gateway/starting-point',
+				caption: 'Main',
+			},
+		},
+	},
 };
