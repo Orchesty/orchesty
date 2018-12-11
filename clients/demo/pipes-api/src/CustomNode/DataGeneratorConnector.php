3a610b464dc4e6397236b58c6f1d5ac91807297b
Radek Bruha <bruha.r@hanaboso.com>
2018-12-11 12:40:04 +0100

    Create DataGeneratorConnector

---
+++ clients/demo/pipes-api/src/CustomNode/DataGeneratorConnector.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Demo\CustomNode;
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+use Hanaboso\CommonsBundle\Process\ProcessDto;
+use Hanaboso\PipesFramework\CustomNode\CustomNodeInterface;
+
+/**
+ * Class DataGeneratorConnector
+ *
+ * @package Demo\CustomNode
+ */
+final class DataGeneratorConnector implements CustomNodeInterface
+{
+
+    /**
+     * @param ProcessDto $dto
+     *
+     * @return ProcessDto
+     * @throws Exception
+     */
+    public function process(ProcessDto $dto): ProcessDto
+    {
+        $data = json_decode($dto->getData(), TRUE);
+        $key  = (new DateTime('NOW', new DateTimeZone('UTC')))->format('d. m. Y H:i:s');
+
+        $data['generator'][$key] = [
+            password_hash($key, PASSWORD_ARGON2I),
+            password_hash($key, PASSWORD_ARGON2I),
+            password_hash($key, PASSWORD_ARGON2I),
+        ];
+
+        /** @var string $data */
+        $data = json_encode($data);
+
+        return $dto->setData($data);
+    }
+
+}
