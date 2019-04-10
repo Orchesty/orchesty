f4a792a826f9d4140a00e9670af927173e1e500e
Radek Bruha <bruha.r@hanaboso.com>
2019-04-10 15:29:41 +0200

    Pipes UI: Implement SelectBox for connectors and custom nodes

--- src/views/elements/bpmn/custom-modeler/PropertiesPanel/parts/CustomNameProps.js
+++ src/views/elements/bpmn/custom-modeler/PropertiesPanel/parts/CustomNameProps.js
@@ -1,6 +1,7 @@
 import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
 import cmdHelper from 'bpmn-js-properties-panel/lib/helper/CmdHelper';
 import { is, getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+import apiGatewayServer from 'services/apiGatewayServer';
 
 export default function (group, element, translate) {
   if (is(element, 'bpmn:Collaboration')) {
@@ -13,30 +14,66 @@ export default function (group, element, translate) {
     modelProperty = 'text';
   }
 
-  group.entries.push(entryFactory.validationAwareTextField({
-    id: 'name',
-    label: 'Name',
-    modelProperty,
-    validate: (element, values) => {
-      if (element.type === 'bpmn:Process') {
+  const pipesType = (getBusinessObject(element).pipesType);
+  const type = pipesType === 'custom' ? 'custom_node' : 'connector';
+  const typeKey = `pipes-${type}-list`;
+
+  if (['connector', 'batch_connector', 'custom'].includes(pipesType)) {
+    apiGatewayServer(() => {}, 'GET', `/nodes/${type}/list_nodes`, null).then(response => {
+      localStorage.setItem(typeKey, JSON.stringify(response));
+    });
+
+    const selectOptions = JSON.parse(localStorage.getItem(typeKey) || '[]');
+
+    group.entries.push(entryFactory.selectBox({
+      id: 'names',
+      label: 'Names',
+      selectOptions: selectOptions.map(item => ({ name: item, value: item })),
+      modelProperty,
+      validate: (element, values) => {
+        if (element.type === 'bpmn:Process') {
+          return {};
+        }
+
+        if (!values.name || values.name === 'Custom') {
+          return { name: 'Name must not be empty.' };
+        }
+
         return {};
-      }
+      },
+      getProperty(element) {
+        return getBusinessObject(element).name;
+      },
+      setProperty(element, properties) {
+        return cmdHelper.updateProperties(element, properties);
+      },
+    }));
+  } else {
+    group.entries.push(entryFactory.validationAwareTextField({
+      id: 'name',
+      label: 'Name',
+      modelProperty,
+      validate: (element, values) => {
+        if (element.type === 'bpmn:Process') {
+          return {};
+        }
 
-      if (/\s/.test(values.name)) {
-        return { name: 'Name must not contain spaces.' };
-      }
+        if (/\s/.test(values.name)) {
+          return { name: 'Name must not contain spaces.' };
+        }
 
-      if (!values.name) {
-        return { name: 'Name must not be empty.' };
-      }
+        if (!values.name) {
+          return { name: 'Name must not be empty.' };
+        }
 
-      return {};
-    },
-    getProperty(element) {
-      return getBusinessObject(element).name;
-    },
-    setProperty(element, properties) {
-      return cmdHelper.updateProperties(element, properties);
-    },
-  }));
+        return {};
+      },
+      getProperty(element) {
+        return getBusinessObject(element).name;
+      },
+      setProperty(element, properties) {
+        return cmdHelper.updateProperties(element, properties);
+      },
+    }));
+  }
 }
