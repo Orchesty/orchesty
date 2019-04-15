7b1c9f287917f9733d3698b5bd6fb38102bd035f
Dominik Mosner <mosner.d@hanaboso.com>
2019-04-15 16:02:03 +0200

    Topology installer

---
+++ tests/Integration/TopologyInstaller/CategoryParserTest.php
@@ -0,0 +1,116 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration\TopologyInstaller;
+
+use Exception;
+use Hanaboso\PipesFramework\Category\Document\Category;
+use Hanaboso\PipesFramework\Configurator\Document\Topology;
+use Hanaboso\PipesFramework\TopologyInstaller\CategoryParser;
+use Hanaboso\PipesFramework\TopologyInstaller\Dto\TopologyFile;
+use RuntimeException;
+use Tests\DatabaseTestCaseAbstract;
+
+/**
+ * Class CategoryParserTest
+ *
+ * @package Tests\Integration\TopologyInstaller
+ */
+final class CategoryParserTest extends DatabaseTestCaseAbstract
+{
+
+    /**
+     * @throws Exception
+     */
+    public function testClassifyTopology(): void
+    {
+        $categoryManager = $this->ownContainer->get('hbpf.configurator.manager.category');
+
+        $categoryParser = new CategoryParser($this->dm, $categoryManager);
+        $categoryParser->addRoot('System', '*/data');
+        $categoryParser->addAlias('System', 'folder', 'Folder-Cat');
+        $categoryParser->addAlias('System', 'new', 'neu');
+        $categoryParser->addExclude('System', 'inner');
+
+        $categoryParser->addRoot('pipes-topo', '/var/www/html/system/topologies');
+
+        $categoryParser->addRoot('another-topos', '/var/www/html/system/impl');
+        $categoryParser->addAlias('another-topos', 'folder', 'just-a-folder');
+        $categoryParser->addExclude('another-topos', 'inner');
+
+        $topo = new Topology();
+        $this->dm->persist($topo);
+        $topo2 = new Topology();
+        $this->dm->persist($topo2);
+        $topo3 = new Topology();
+        $this->dm->persist($topo3);
+        $topo4 = new Topology();
+        $this->dm->persist($topo4);
+        $this->dm->flush();
+
+        $file = new TopologyFile('aaa.tplg', '/var/www/aa/data/inner/SystemXYZ/SystemXYZ/folder');
+        $categoryParser->classifyTopology($topo, $file);
+
+        $file = new TopologyFile('bbb.tplg', '/var/www/aa/data/inner/SystemAAA/new');
+        $categoryParser->classifyTopology($topo2, $file);
+
+        $file = new TopologyFile('ccc.tplg', '/var/www/html/system/topologies');
+        $categoryParser->classifyTopology($topo3, $file);
+
+        $file = new TopologyFile('ddd.tplg', '/var/www/html/system/impl/inner/folder');
+        $categoryParser->classifyTopology($topo4, $file);
+
+        // Check Roots
+        $root  = $this->dm->getRepository(Category::class)->findOneBy(['name' => 'System']);
+        $root2 = $this->dm->getRepository(Category::class)->findOneBy(['name' => 'pipes-topo']);
+        $root3 = $this->dm->getRepository(Category::class)->findOneBy(['name' => 'another-topos']);
+        self::assertInstanceOf(Category::class, $root);
+        self::assertInstanceOf(Category::class, $root2);
+        self::assertInstanceOf(Category::class, $root3);
+
+        // Check sub-cat of first root
+        $subRoot = $this->dm->getRepository(Category::class)->findOneBy(
+            ['name' => 'SystemXYZ', 'parent' => $root->getId()]
+        );
+        self::assertInstanceOf(Category::class, $subRoot);
+        $sub2Root = $this->dm->getRepository(Category::class)->findOneBy(
+            ['name' => 'Folder-Cat', 'parent' => $subRoot->getId()]
+        );
+        self::assertInstanceOf(Category::class, $sub2Root);
+
+        $subRoot = $this->dm->getRepository(Category::class)->findOneBy(
+            ['name' => 'SystemAAA', 'parent' => $root->getId()]
+        );
+        self::assertInstanceOf(Category::class, $subRoot);
+        $sub2Root = $this->dm->getRepository(Category::class)->findOneBy(
+            ['name' => 'neu', 'parent' => $subRoot->getId()]
+        );
+        self::assertInstanceOf(Category::class, $sub2Root);
+
+        // Check sub-cat of third root
+        $subRoot = $this->dm->getRepository(Category::class)->findOneBy(
+            ['name' => 'just-a-folder', 'parent' => $root3->getId()]
+        );
+        self::assertInstanceOf(Category::class, $subRoot);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testClassifyTopologyError(): void
+    {
+        $categoryManager = $this->ownContainer->get('hbpf.configurator.manager.category');
+
+        $categoryParser = new CategoryParser($this->dm, $categoryManager);
+        $categoryParser->addRoot('System', '*/*');
+
+        $topo = new Topology();
+        $this->dm->persist($topo);
+        $this->dm->flush();
+
+        $file = new TopologyFile('aaa.tplg', '/var/www/aa/data/inner/SystemXYZ/SystemXYZ/folder');
+
+        $this->expectException(RuntimeException::class);
+        $categoryParser->classifyTopology($topo, $file);
+    }
+
+}
---
+++ tests/Unit/TopologyInstaller/InstallManagerTest.php
@@ -0,0 +1,154 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\TopologyInstaller;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
+use Hanaboso\CommonsBundle\Transport\Curl\Dto\ResponseDto;
+use Hanaboso\PipesFramework\Configurator\Document\Topology;
+use Hanaboso\PipesFramework\Configurator\Exception\TopologyException;
+use Hanaboso\PipesFramework\Configurator\Model\TopologyManager;
+use Hanaboso\PipesFramework\Configurator\Repository\TopologyRepository;
+use Hanaboso\PipesFramework\Connector\Exception\ConnectorException;
+use Hanaboso\PipesFramework\HbPFConfiguratorBundle\Handler\RequestHandler;
+use Hanaboso\PipesFramework\TopologyInstaller\CategoryParser;
+use Hanaboso\PipesFramework\TopologyInstaller\Dto\CompareResultDto;
+use Hanaboso\PipesFramework\TopologyInstaller\Dto\TopologyFile;
+use Hanaboso\PipesFramework\TopologyInstaller\Dto\UpdateObject;
+use Hanaboso\PipesFramework\TopologyInstaller\InstallManager;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Predis\Client;
+use ReflectionException;
+use Tests\PrivateTrait;
+
+/**
+ * Class InstallManagerTest
+ *
+ * @package Tests\Unit\TopologyInstaller
+ */
+final class InstallManagerTest extends TestCase
+{
+
+    use PrivateTrait;
+
+    /**
+     * @throws ConnectorException
+     * @throws MongoDBException
+     * @throws ReflectionException
+     * @throws TopologyException
+     */
+    public function testPrepareInstall(): void
+    {
+        $topo = new Topology();
+        $data = $this->createRedisRecord();
+
+        $manager = $this->createManager($data, $topo, []);
+        $output  = $manager->prepareInstall(TRUE, TRUE, TRUE);
+        self::assertTrue(is_array($output));
+        self::assertArrayHasKey('create', $output);
+        self::assertArrayHasKey('update', $output);
+        self::assertArrayHasKey('delete', $output);
+    }
+
+    /**
+     * @throws ConnectorException
+     * @throws ReflectionException
+     */
+    public function testMakeInstall(): void
+    {
+        $topo = new Topology();
+        $this->setProperty($topo, 'id', '123');
+        $data = $this->createRedisRecord();
+
+        $manager = $this->createManager($data, $topo, []);
+        $output  = $manager->makeInstall(TRUE, TRUE, TRUE);
+        self::assertTrue(is_array($output));
+        self::assertArrayHasKey('create', $output);
+        self::assertArrayHasKey('update', $output);
+        self::assertArrayHasKey('delete', $output);
+        self::assertNotEmpty($output['delete']);
+    }
+
+    /**
+     * @throws ConnectorException
+     * @throws ReflectionException
+     */
+    public function testMakeInstallEx(): void
+    {
+        $topo = new Topology();
+        $this->setProperty($topo, 'id', '123');
+
+        $manager = $this->createManager(NULL, $topo, []);
+        $this->expectException(ConnectorException::class);
+        $manager->makeInstall(TRUE, TRUE, TRUE);
+    }
+
+    /**
+     * @param string|null $redisResult
+     * @param Topology    $savedTopo
+     * @param array       $dirs
+     *
+     * @return InstallManager
+     * @throws ReflectionException
+     */
+    private function createManager(
+        ?string $redisResult,
+        Topology $savedTopo,
+        array $dirs = []
+    ): InstallManager
+    {
+        /** @var DocumentManager|MockObject $dm */
+        $repo = $this->createMock(TopologyRepository::class);
+        $dm   = $this->createMock(DocumentManager::class);
+        $dm->method('getRepository')->willReturn($repo);
+        $dm->method('persist')->willReturn(TRUE);
+
+        /** @var Client|MockObject $client */
+        $client = $this->getMockBuilder(Client::class)->setMethods(['set', 'get', 'del'])->getMock();
+        $client->method('set')->willReturn(TRUE);
+        $client->method('get')->willReturn($redisResult);
+        $client->method('del')->willReturn(TRUE);
+
+        /** @var TopologyManager|MockObject $topologyManager */
+        $topologyManager = $this->createMock(TopologyManager::class);
+        $topologyManager->method('createTopology')->willReturn(new Topology());
+        $topologyManager->method('publishTopology')->willReturn(TRUE);
+        $topologyManager->method('updateTopology')->willReturn($savedTopo);
+        $topologyManager->method('deleteTopology')->willReturn(TRUE);
+        $topologyManager->method('saveTopologySchema')->willReturn($savedTopo);
+
+        /** @var RequestHandler|MockObject $requestHandler */
+        $requestHandler = $this->createMock(RequestHandler::class);
+        $requestHandler->method('runTopology')->willReturn(new ResponseDto(200, '', '', []));
+        $requestHandler->method('deleteTopology')->willReturn(new ResponseDto(200, '', '', []));
+
+        /** @var CategoryParser|MockObject $categoryParser */
+        $categoryParser = $this->createMock(CategoryParser::class);
+        $categoryParser->method('classifyTopology')->willReturn(NULL);
+
+        return new InstallManager($dm, $client, $topologyManager, $requestHandler, $categoryParser, $dirs);
+    }
+
+    /**
+     * @return string
+     * @throws ReflectionException
+     */
+    private function createRedisRecord(): string
+    {
+        $topo = new Topology();
+        $topo->setName('file-upl');
+
+        $topo2 = new Topology();
+        $topo2->setName('file-del');
+        $this->setProperty($topo2, 'id', '345');
+
+        $dto = new CompareResultDto();
+        $dto->addCreate(new TopologyFile('file.tplg', __DIR__ . '/data/file.tplg'));
+        $dto->addUpdate(new UpdateObject($topo, new TopologyFile('file-upl.tplg', __DIR__ . '/data/file-upl.tplg')));
+        $dto->addDelete([$topo2]);
+
+        return serialize($dto);
+    }
+
+}
