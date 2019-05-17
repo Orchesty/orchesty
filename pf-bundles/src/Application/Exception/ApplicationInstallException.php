9781b23578ca0f22306efc952556f956fdebb7ab
Dominik Mosner <mosner.d@hanaboso.com>
2019-05-17 09:23:35 +0200

    Created helper class for getSettingsField

--- pf-bundles/src/Application/Exception/ApplicationInstallException.php
+++ pf-bundles/src/Application/Exception/ApplicationInstallException.php
@@ -16,5 +16,6 @@ final class ApplicationInstallException extends PipesFrameworkExceptionAbstract
 
     public const APP_ALREADY_INSTALLED = self::OFFSET + 1;
     public const APP_WAS_NOT_FOUND     = self::OFFSET + 2;
+    public const INVALID_FIELD_TYPE    = self::OFFSET + 3;
 
 }
---
+++ pf-bundles/src/Application/Model/Form/Form.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Hanaboso\PipesFramework\Application\Model\Form;
+
+/**
+ * Class Form
+ *
+ * @package Hanaboso\PipesFramework\Application\Model\Form
+ */
+class Form
+{
+
+    /**
+     * @var Field[]
+     */
+    private $fields = [];
+
+    /**
+     * @param Field $field
+     *
+     * @return Form
+     */
+    public function addField(Field $field): Form
+    {
+        $this->fields[] = $field;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $fields = [];
+        foreach ($this->fields as $field) {
+            $fields[] = $field->toArray();
+        }
+
+        return $fields;
+    }
+
+}
