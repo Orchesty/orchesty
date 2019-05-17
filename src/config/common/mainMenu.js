69fa8d848a8c0c3673a6780e84e6c9212f3e6aed
Radek Bruha <bruha.r@hanaboso.com>
2019-05-17 15:58:40 +0200

    PipesFramework: Remove PHP repeater

--- src/config/common/mainMenu.js
+++ src/config/common/mainMenu.js
@@ -20,11 +20,6 @@ export default dispatch => [
   },
   {
     type: menuItemType.ACTION,
-    caption: 'Authorizations',
-    action: () => dispatch(applicationActions.openPage('authorization_list')),
-  },
-  {
-    type: menuItemType.ACTION,
     caption: 'Notifications',
     action: () => dispatch(applicationActions.openPage('notification_settings')),
   },
