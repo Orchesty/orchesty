437225da91ec40791249adec588f250c0a667f96
Radek Jirsa <jirsa.r@hanaboso.com>
2019-06-12 15:49:45 +0200

    FE: updated config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,14 @@ if [ -z "$BACKEND_URL" ]; then
   exit 1
 fi
 
-sed -ri "s|http://url\.to\.api\.gateway|$BACKEND_URL|g" -i /var/www/html/ui/bundle.js
+
+if [ -z "$FRONTEND_URL" ]; then
+  echo "env var FRONTEND_URL not defined"
+  exit 1
+fi
+
+sed -ri "s|http://url\.to\.backend|$BACKEND_URL|g" -i /var/www/html/ui/bundle.js
+sed -ri "s|http://url\.to\.frontend|$FRONTEND_URL|g" -i /var/www/html/ui/bundle.js
 
 upstream_resolver
 upstream_resolver --periodic &
