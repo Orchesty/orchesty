7c65021f8af6ed17fd54641218755f44f1c8a49d
Jakub Husák <husak.j@hanaboso.com>
2019-11-27 16:41:22 +0100

    website: turn on S3 bucket website support, fix deploy env

--- website/terraform/main.tf
+++ website/terraform/main.tf
@@ -14,7 +14,9 @@ provider "aws" {
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.website_bucket_ws[terraform.workspace]
   acl    = "public-read"
-
+  website {
+    index_document = "index.html"
+  }
   tags = {
     tf_stack = local.tf_stack
   }
