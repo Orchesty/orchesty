d78b011971d7002f0be402a692a547dec90b68d3
Katerina Bellerova <bellerova.k@hanaboso.com>
2019-12-09 18:09:23 +0100

    100% coverage bigcommerce tests
    s

--- pipes-connectors/tests/Integration/Model/Application/Impl/Bigcommerce/BigcommerceApplicationTest.php
+++ pipes-connectors/tests/Integration/Model/Application/Impl/Bigcommerce/BigcommerceApplicationTest.php
@@ -3,7 +3,12 @@
 namespace Tests\Integration\Model\Application\Impl\Bigcommerce;
 
 use Exception;
+use Hanaboso\CommonsBundle\Enum\ApplicationTypeEnum;
+use Hanaboso\CommonsBundle\Exception\DateTimeException;
+use Hanaboso\CommonsBundle\Transport\Curl\Dto\RequestDto;
 use Hanaboso\HbPFConnectors\Model\Application\Impl\Bigcommerce\BigcommerceApplication;
+use Hanaboso\PipesPhpSdk\Application\Document\ApplicationInstall;
+use Hanaboso\PipesPhpSdk\Application\Model\Form\Field;
 use Tests\DatabaseTestCaseAbstract;
 use Tests\DataProvider;
 
@@ -15,7 +20,7 @@ use Tests\DataProvider;
 final class BigcommerceApplicationTest extends DatabaseTestCaseAbstract
 {
 
-    private const CLIENT_ID = '1t22ea9p6iyih**********rq9o3xf';
+    private const CLIENT_ID     = '1t22ea9p6iyih**********rq9o3xf';
     private const CLIENT_SECRET = '811a14ca490bbb2cd188cf4bd9bef795b35c9**********737e5b805038fecb4';
 
     /**
@@ -38,7 +43,95 @@ final class BigcommerceApplicationTest extends DatabaseTestCaseAbstract
         );
 
         $this->pf($applicationInstall);
+        $this->assertEquals(TRUE, $bigcommerceApplication->isAuthorized($applicationInstall));
         $bigcommerceApplication->authorize($applicationInstall, ['store_v2_products']);
+
+    }
+
+    /**
+     * @throws DateTimeException
+     */
+    public function testIsAuthorizedNoToken(): void
+    {
+        $bigcommerceApplication = self::$container->get('hbpf.application.bigcommerce');
+        $applicationInstall     = new ApplicationInstall();
+        $this->pf($applicationInstall);
+        $this->assertEquals(FALSE, $bigcommerceApplication->isAuthorized($applicationInstall));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRequestDto(): void
+    {
+        $bigcommerceApplication = self::$container->get('hbpf.application.bigcommerce');
+        $applicationInstall     = DataProvider::getOauth2AppInstall(
+            $bigcommerceApplication->getKey(),
+            'user',
+            'token',
+            self::CLIENT_ID,
+            self::CLIENT_SECRET
+        );
+        $this->pf($applicationInstall);
+        $dto = $bigcommerceApplication->getRequestDto(
+            $applicationInstall,
+            'POST',
+            'url',
+            '{"data":"hello data"}'
+        );
+        $this->assertInstanceOf(RequestDto::class, $dto);
+        $this->assertEquals('{"data":"hello data"}', $dto->getBody());
+    }
+
+    /**
+     *
+     */
+    public function testGetApplicationType(): void
+    {
+        $bigcommerce = self::$container->get('hbpf.application.bigcommerce');
+        self::assertEquals(
+            ApplicationTypeEnum::WEBHOOK,
+            $bigcommerce->getApplicationType()
+        );
+    }
+
+    /**
+     *
+     */
+    public function testName(): void
+    {
+        $bigcommerce = self::$container->get('hbpf.application.bigcommerce');
+        self::assertEquals(
+            'Bigcommerce',
+            $bigcommerce->getName()
+        );
+    }
+
+    /**
+     *
+     */
+    public function testGetDescription(): void
+    {
+        $bigcommerce = self::$container->get('hbpf.application.bigcommerce');
+        self::assertEquals(
+            'Bigcommerce v1',
+            $bigcommerce->getDescription()
+        );
+    }
+
+    /**
+     *
+     */
+    public function testGetSettingsForm(): void
+    {
+        $bigcommerce = self::$container->get('hbpf.application.bigcommerce');
+
+        $fields = $bigcommerce->getSettingsForm()->getFields();
+        foreach ($fields as $field) {
+            self::assertInstanceOf(Field::class, $field);
+            self::assertContains($field->getKey(), ['client_id', 'client_secret']);
+        }
+
     }
 
 }
