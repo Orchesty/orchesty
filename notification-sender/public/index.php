53d65b9ff2b71cf8b8b43e8bb65fcee3633e490a
Radek Bruha <bruha.r@hanaboso.com>
2020-03-19 11:16:17 +0100

    NotificationSender: Create StatusController and Swagger API

--- notification-sender/public/index.php
+++ notification-sender/public/index.php
@@ -1,37 +1,27 @@
 <?php
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Hanaboso\NotificationSender\Kernel;
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__ . '/../.env');
-}
-
 if ($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev'))) {
     umask(0000);
-
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? FALSE) {
-    Request::setTrustedProxies(explode(',', $trustedProxies),
-        Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? FALSE) {
-    Request::setTrustedHosts(explode(',', $trustedHosts));
-}
-
+// Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
 $kernel   = new Kernel($_SERVER['APP_ENV'] ?? 'dev',
-    $_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev')));
+                       $_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev')));
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
