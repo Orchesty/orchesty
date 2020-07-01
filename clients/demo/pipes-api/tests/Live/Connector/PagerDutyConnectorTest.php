b9b3b83db15768c05eac9dab3fa0447c8073ffcf
Radek Bruha <bruha.r@hanaboso.com>
2020-07-01 10:20:50 +0200

    Create PagerDutyConnectorTest

---
+++ clients/demo/pipes-api/tests/Live/Connector/PagerDutyConnectorTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace DemoTests\Live\Connector;
+
+use Demo\Connector\PagerDutyConnector;
+use Exception;
+use Hanaboso\CommonsBundle\Process\ProcessDto;
+use Hanaboso\Utils\String\Json;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class PagerDutyConnectorTest
+ *
+ * @package DemoTests\Live\Connector
+ */
+final class PagerDutyConnectorTest extends KernelTestCase
+{
+
+    /**
+     * @group live
+     *
+     * @throws Exception
+     */
+    public function testProcessAction(): void
+    {
+        $manager   = self::$container->get('hbpf.transport.curl_manager');
+        $connector = new PagerDutyConnector($manager);
+        $dto       = new ProcessDto();
+        $dto->setData(Json::encode(['since' => '2020-06-01', 'until' => '2020-06-24']));
+        $data = $connector->processAction($dto)->getData();
+        $arr  = Json::decode($data);
+        self::assertEquals(56, $arr['Radek Jirsa']['hours']);
+        self::assertEquals(55, $arr['Marcel Pavlíček']['hours']);
+        self::assertEquals(40, $arr['Tomáš Procházka']['hours']);
+        self::assertEquals(45, $arr['Václav Krecl']['hours']);
+        self::assertEquals(42, $arr['Jakub Husák']['hours']);
+    }
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
+
+}
