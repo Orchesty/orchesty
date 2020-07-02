024a246f50c432d7fad289909a6277ee48323978
Jakub Husák <husak.j@hanaboso.com>
2020-07-02 18:21:25 +0200

    website terraform: CloudFront config

--- website/terraform/main.tf
+++ website/terraform/main.tf
@@ -1,5 +1,14 @@
 variable "region" {}
+
 variable "website_bucket_ws" {
+    type = map
+}
+
+variable "website_aliases_ws" {
+    type = map
+}
+
+variable "website_certificate_arn_ws" {
     type = map
 }
 
@@ -18,6 +27,59 @@ resource "aws_s3_bucket" "website_bucket" {
     index_document = "index.html"
   }
   tags = {
-    tf_stack = local.tf_stack
+    Stack = local.tf_stack
+  }
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.website_endpoint
+    origin_id   = "pipes-website-${terraform.workspace}"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = var.website_aliases_ws[terraform.workspace]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "pipes-website-${terraform.workspace}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60 #3600
+    max_ttl                = 60 #86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+    acm_certificate_arn = var.website_certificate_arn_ws[terraform.workspace]
+  }
+
+  tags = {
+    Stack = local.tf_stack
   }
 }
--- website/terraform/terraform.tfvars
+++ website/terraform/terraform.tfvars
@@ -4,3 +4,14 @@ website_bucket_ws = {
     stage = "pipes-website-stage"
     prod = "pipes-website-prod"
 }
+
+website_aliases_ws = {
+    stage = ["pipes-doc.hanaboso.com"]
+    prod = ["pipes-doc.hanaboso.com"]
+}
+
+# Must be located in us-east-1!!!
+website_certificate_arn_ws = {
+    stage = "arn:aws:acm:us-east-1:149082520595:certificate/bdb8f270-f35b-4056-8be2-8ac71c074bc7"
+    prod = "arn:aws:acm:us-east-1:149082520595:certificate/bdb8f270-f35b-4056-8be2-8ac71c074bc7"
+}
