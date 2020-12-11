a020c1345c4fb9a77134c782b619a189ba1a163e
Radek Jirsa <jirsa.r@hanaboso.com>
2020-12-11 13:50:06 +0100

    Allowed repeater in batch

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ if [ "$MONGO_HOST" != "" ]; then
         mongo --host="$MONGO_HOST" "$MONGO_DATABASE" --eval="db.createCollection( '$MONGO_COLLECTION', { capped: true, size: $MONGO_SIZE_COLLETION } );"
     fi;
 
+    mongo --host="$MONGO_HOST" "$MONGO_DATABASE" --eval="db.$MONGO_COLLECTION.createIndex({'pipes.severity': 1});db.$MONGO_COLLECTION.createIndex({'pipes.correlation_id': 1});db.$MONGO_COLLECTION.createIndex({'@timestamp': -1});"
+
 fi
 
 exec /usr/local/bin/docker-entrypoint "$@"
