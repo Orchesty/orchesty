84a82e693b0feb601e6183b1545f1ff283d79979
Radek Bruha <bruha.r@hanaboso.com>
2021-02-15 09:10:41 +0100

    PF Bundles: Allow configuration of installer_dirs thru ENV variable

--- pf-bundles/src/HbPFLogsBundle/DependencyInjection/Compiler/LogsCompilerPass.php
+++ pf-bundles/src/HbPFLogsBundle/DependencyInjection/Compiler/LogsCompilerPass.php
@@ -27,6 +27,7 @@ final class LogsCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container): void
     {
+        /** @var mixed[] $config */
         $config = $container->getParameter(HbPFLogsBundle::KEY);
 
         $dm                   = $container->getDefinition('doctrine_mongodb.odm.default_document_manager');
