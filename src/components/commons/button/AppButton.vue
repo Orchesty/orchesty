6c470fbb4f02d118747aefa1cb3b910aa008c66c
Karel Mistrik <mistrik.k@hanaboso.com>
2022-01-15 03:05:02 +0100

    typography + app layout fix

---
+++ src/components/commons/button/AppButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-btn v-if="isSending" :class="customClass" :color="color" type="submit" disabled :text="flat">
+    {{ sendingTitle }}
+  </v-btn>
+  <v-btn v-else :class="customClass" :color="color" :disabled="disabled" :text="flat" @click="onClick">
+    {{ buttonTitle }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    isSending: {
+      type: Boolean,
+      required: true,
+    },
+    sendingTitle: {
+      type: String,
+      required: true,
+    },
+    buttonTitle: {
+      type: String,
+      required: true,
+    },
+    onClick: {
+      type: Function,
+      required: true,
+    },
+    disabled: Boolean,
+    color: {
+      type: String,
+      required: false,
+      default: 'primary',
+    },
+    flat: {
+      type: Boolean,
+      default: true,
+    },
+    customClass: {
+      type: String,
+      required: false,
+      default: () => '',
+    },
+  },
+}
+</script>
