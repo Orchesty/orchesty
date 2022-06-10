d20cad55d690aefc411609a562deee7cb31e1e5e
Radek Jirsa <jirsa.r@hanaboso.com>
2022-06-10 10:33:57 +0200

    New ui

--- pkg/storage/topology.go
+++ pkg/storage/topology.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Topology represents topology
 type Topology struct {
-	ID      primitive.ObjectID `bson:"_id"json:"id"`
-	Name    string             `bson:"name"json:"name"`
-	Version int32              `bson:"version"json:"version"`
+	ID      primitive.ObjectID `bson:"_id" json:"id"`
+	Name    string             `bson:"name" json:"name"`
+	Version int32              `bson:"version" json:"version"`
 	Node    *Node              `json:"node"`
 }
