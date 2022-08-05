daf9b0d2da2b025f4a1c4236fcb80fa3c496e296
Václav Krecl <krecl.v@hanaboso.com>
2022-08-05 09:06:10 +0000

    New ui

---
+++ pkg/rabbit/publisher.go
@@ -0,0 +1,94 @@
+package rabbit
+
+import (
+	"context"
+	"errors"
+	"github.com/hanaboso/pipes/counter/pkg/config"
+	"github.com/streadway/amqp"
+	"os"
+	"time"
+)
+
+type Publisher struct {
+	Exchange      string
+	RoutingKey    string
+	channel       *amqp.Channel
+	rabbit        *RabbitMq
+	notifyConfirm chan amqp.Confirmation
+}
+
+// Keep it synced or add Lock
+func (p *Publisher) Publish(msg amqp.Publishing) {
+	for {
+		if err := p.channel.Publish(p.Exchange, p.RoutingKey, false, false, msg); err != nil {
+			config.Log.Error(err)
+			p.connect()
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		select {
+		case confirm, ok := <-p.notifyConfirm:
+			if !ok {
+				config.Log.Error(errors.New("publisher's channel is closed"))
+				p.connect()
+				cancel()
+				continue
+			}
+			if !confirm.Ack {
+				config.Log.Error(errors.New("delivery nack"))
+				p.connect()
+				cancel()
+				continue
+			}
+			cancel()
+			return
+		case <-ctx.Done():
+			config.Log.Error(errors.New("publish timeout"))
+			continue
+		}
+	}
+}
+
+func (p *Publisher) connect() {
+	for {
+		if p.channel != nil {
+			_ = p.channel.Close()
+		}
+
+		ch := p.rabbit.channel()
+		p.channel = ch
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		go func() {
+			<-ctx.Done()
+			if err := ctx.Err(); err == context.DeadlineExceeded {
+				// This is hack to work around Go lang's rabbitMq shortcomings
+				// It's a fallback due to amqp unresponsiveness
+				// For example ex-declare with missing hash-ex plugin will do nothing (no error, timeout, nada)
+				config.Log.Error(err)
+				os.Exit(1)
+			}
+		}()
+
+		if _, err := ch.QueueDeclare(p.RoutingKey, true, false, false, false, nil); err != nil {
+			config.Log.Error(err)
+			cancel()
+			continue
+		}
+
+		_ = ch.Confirm(false)
+		p.notifyConfirm = ch.NotifyPublish(make(chan amqp.Confirmation, 20))
+
+		cancel()
+		return
+	}
+}
+
+func (p *Publisher) stop() {
+	if p.channel != nil {
+		if err := p.channel.Close(); err != nil {
+			config.Log.Error(err)
+		}
+	}
+}
