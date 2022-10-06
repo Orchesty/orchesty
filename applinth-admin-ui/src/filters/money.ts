7bcd22c92053f210a06c1e3fe8856e8cf4392b5e
Lukas Trumm <lukas.trumm@gmail.com>
2022-10-06 13:28:12 +0200

    Fix format that is not supported by vue-cli

--- applinth-admin-ui/src/filters/money.ts
+++ applinth-admin-ui/src/filters/money.ts
@@ -2,7 +2,7 @@ export function toCZK(number: number | null): string {
   if (number == null) {
     return "";
   }
-  const normalizedNumber = number / 100_000;
+  const normalizedNumber = number / 100000;
   return Intl.NumberFormat("cs", {
     style: "currency",
     currency: "CZK",
