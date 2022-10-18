6f5b71c0282643960791c1e71aee647f540abe1f
David Horacek <horacek.d@hanaboso.com>
2022-10-18 12:37:43 +0200

    status - adds json to bson keys

--- counter/pkg/model/status_message.go
+++ counter/pkg/model/status_message.go
@@ -1,21 +1,21 @@
 package model
 
 type StatusMessage struct {
-	Type     string                 `bson:"type"`
-	Data     StatusMessageData      `bson:"data"`
-	Contents []StatusMessageContent `bson:"contents"`
+	Type     string                 `bson:"type" json:"type"`
+	Data     StatusMessageData      `bson:"data" json:"data"`
+	Contents []StatusMessageContent `bson:"contents" json:"contents"`
 }
 
 type StatusMessageData struct {
-	TopologyId    string `bson:"topologyId"`
-	ResultMessage string `bson:"resultMessage"`
-	CorrelationId string `bson:"correlationId"`
-	ProcessId     string `bson:"processId"`
-	User          string `bson:"user"`
-	TimestampMs   int64  `bson:"timestampMs"`
+	TopologyId    string `bson:"topologyId" json:"topologyId"`
+	ResultMessage string `bson:"resultMessage" json:"resultMessage"`
+	CorrelationId string `bson:"correlationId" json:"correlationId"`
+	ProcessId     string `bson:"processId" json:"processId"`
+	User          string `bson:"user" json:"user"`
+	TimestampMs   int64  `bson:"timestampMs" json:"timestampMs"`
 }
 
 type StatusMessageContent struct {
-	TrashId *string `bson:"trashId"`
-	Body    string  `bson:"body"`
+	TrashId *string `bson:"trashId" json:"trashId"`
+	Body    string  `bson:"body" json:"body"`
 }
