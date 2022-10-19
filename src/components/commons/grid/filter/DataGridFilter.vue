b68f940cbc0b651258fd1d5257c49a6ee50d748c
Karel Mistrík <mistrik.k@hanaboso.com>
2022-10-19 11:42:05 +0000

    FE Orchesty: SimpleFilter Component refactor PIP-1367

--- src/components/commons/grid/filter/DataGridFilter.vue
+++ src/components/commons/grid/filter/DataGridFilter.vue
@@ -11,40 +11,19 @@
         :is-loading="isLoading"
         :class="{ 'mb-3': simpleFilter }"
       >
-        <template v-if="!simpleFilter" #resetClearButtons="{}">
-          <v-btn color="primary" icon @click="reload">
+        <template v-if="!simpleFilter">
+          <v-btn color="primary" icon @click="onChangeFilter">
             <v-icon> mdi-reload </v-icon>
           </v-btn>
-          <!--          <v-btn color="primary" icon @click="clear(onClearButton)">-->
-          <!--            <v-icon> mdi-close </v-icon>-->
-          <!--          </v-btn>-->
-        </template>
-        <template #headers>
-          <slot name="headers"></slot>
         </template>
       </quick-grid-filter>
 
       <simple-grid-filter
         v-if="simpleFilter"
-        ref="simpleGridFilter"
-        :key="key"
         :simple-filter-enum="simpleFilterEnum"
-        :headers="headers"
-        :filter="filter"
-        :filter-meta="filterMeta"
-        :on-change="onChangeFilter"
         :class="{ 'mt-2': disableFilter }"
-        @sendFilter="sendFilter"
-      >
-        <template #resetClearButtons="{ onClearButton }">
-          <v-btn class="ml-2" color="primary" icon @click="reload">
-            <v-icon> mdi-reload </v-icon>
-          </v-btn>
-          <v-btn class="ml-1" color="primary" icon @click="clear(onClearButton)">
-            <v-icon> mdi-close </v-icon>
-          </v-btn>
-        </template>
-      </simple-grid-filter>
+        @onSendFilter="sendFilter"
+      />
     </v-col>
   </v-row>
 </template>
@@ -56,7 +35,6 @@ import { FILTER } from '@/services/enums/gridEnums'
 import { GRID } from '@/store/modules/grid/types'
 import { mapActions } from 'vuex'
 import { DATA_GRIDS } from '@/services/enums/dataGridEnums'
-import { SIMPLE_FILTER } from '@/services/enums/dataGridFilterEnums'
 
 export default {
   name: 'DataGridFilter',
@@ -77,18 +55,6 @@ export default {
     disableFilter: {
       type: Boolean,
       default: false,
-    },
-    onFilter: {
-      type: Function,
-      required: true,
-    },
-    onReload: {
-      type: Function,
-      required: true,
-    },
-    onClear: {
-      type: Function,
-      required: true,
     },
     headers: {
       type: Array,
@@ -111,69 +77,32 @@ export default {
     return {
       FILTER,
       currentFilter: [],
+      currentSearch: null,
+      currentQuickFilter: [],
       currentMeta: {},
-      key: 0,
     }
-  },
-  computed: {
-    filterCount() {
-      let i = 0
-      this.filter.forEach((and) => {
-        and.forEach(() => {
-          let defaultFilter = false
-          and.forEach((item) => {
-            item.default ? (defaultFilter = true) : (defaultFilter = false)
-          })
-          if (!defaultFilter) {
-            i++
-          }
-        })
-      })
-
-      return i
-    },
   },
   methods: {
     ...mapActions(DATA_GRIDS.USER_TASK, [GRID.ACTIONS.GRID_FETCH]),
     onChangeFilter(filter, meta) {
-      this.currentFilter = filter
+      this.currentQuickFilter = filter
       this.currentMeta = meta
 
-      if (meta.type === FILTER.QUICK_FILTER) this.sendFilter()
+      if (meta.type === FILTER.QUICK_FILTER)
+        this.$emit('onFetchGrid', {
+          search: this.currentSearch,
+          filter: [].concat(this.currentQuickFilter, this.currentFilter),
+          paging: null,
+          sorter: null,
+        })
     },
-    clear() {
-      this.$refs.quickGridFilter.onClear()
-      if (this.filter.length === 0) {
-        this.key++
-      }
+    async sendFilter(params) {
+      this.currentFilter = params.filter
+      this.currentSearch = params.search
 
-      this.$refs.simpleGridFilter.logsFilterValues.fullTextSearch = null
-      this.$refs.simpleGridFilter.logsFilterValues.timeMargin = 0
-      this.currentFilter = []
-      this.currentMeta = {}
-      this.onClear()
-    },
-    async reload() {
-      await this.onReload()
-    },
-    async sendFilter() {
-      if (SIMPLE_FILTER.LOGS === this.simpleFilterEnum) {
-        this.onFilter(
-          this.$refs.simpleGridFilter.logsFilterValues.fullTextSearch,
-          null,
-          [].concat(this.currentFilter, this.$refs.simpleGridFilter.logsFilter),
-          null,
-          null
-        )
-      } else if (SIMPLE_FILTER.TRASH === this.simpleFilterEnum) {
-        const fieldFilters = this.$refs.simpleGridFilter.trashFilter || []
-        this.onFilter(null, null, [].concat(this.currentFilter, fieldFilters), null, null)
-      } else if (SIMPLE_FILTER.USER_TASK === this.simpleFilterEnum) {
-        const fieldFilters = this.$refs.simpleGridFilter.userTaskFilter || []
-        this.onFilter(null, null, [].concat(this.currentFilter, fieldFilters), null, null)
-      } else {
-        this.onFilter(null, null, this.currentFilter, null, null)
-      }
+      params.filter = [].concat(this.currentFilter, this.currentQuickFilter)
+
+      this.$emit('onFetchGrid', params)
     },
   },
 }
