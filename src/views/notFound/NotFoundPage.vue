0cf78032d142826280c919290133384c447d511a
Karel Mistrík <mistrik.k@hanaboso.com>
2022-10-25 13:54:53 +0200

    not found page redesign and translation added

--- src/views/notFound/NotFoundPage.vue
+++ src/views/notFound/NotFoundPage.vue
@@ -1,16 +1,19 @@
 <template>
   <auth-layout>
-    <div class="text-center">
-      <h1>{{ $t("notFound.title") }}</h1>
-    </div>
+    <auth-split-layout>
+      <template #form>
+        <h1 class="text-center">{{ $t("page.text.pageNotFound") }}</h1>
+      </template>
+    </auth-split-layout>
   </auth-layout>
 </template>
 
 <script>
+import AuthSplitLayout from "@/components/app/auth/layout/AuthSplitLayout"
 import AuthLayout from "@/components/layout/auth/AuthLayout"
 
 export default {
   name: "NotFoundPage",
-  components: { AuthLayout },
+  components: { AuthLayout, AuthSplitLayout },
 }
 </script>
