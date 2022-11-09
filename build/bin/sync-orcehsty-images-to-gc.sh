3d7a2b49e8cd12d2b34bec3ba6afd7957f345619
Jakub Husák <husak.j@gmail.com>
2022-11-09 07:23:09 +0100

    tooling: add cript to sync Orchesty images to GCloud AR

---
+++ build/bin/sync-orcehsty-images-to-gc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+TAG=$1
+
+IMAGES="\
+	backend \
+	bridge \
+	counter \
+	cron \
+	detector \
+	frontend \
+	limiter \
+	fluentd \
+	pf-bridge \
+	starting-point \
+	topology-api \
+"
+
+TOKEN=$(gcloud auth print-access-token)
+
+for I in $IMAGES; do
+	echo $I
+	docker run -t --rm quay.io/skopeo/stable:latest copy --dest-creds oauth2accesstoken:${TOKEN} docker://orchesty/${I}:${TAG} docker://europe-west1-docker.pkg.dev/orchesty-cloud-prod/orchesty/${I}:${TAG}
+done
