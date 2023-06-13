5b0121cc861d93e6536d01165cd7fa516a7725b0
Fila <fila.r@hanaboso.com>
2023-06-13 15:38:43 +0200

    Update USCCP

--- saas/usccp/src/controllers/__tests__/eventController.ts
+++ saas/usccp/src/controllers/__tests__/eventController.ts
@@ -12,9 +12,8 @@ describe('eventController', () => {
         const resp = await supertest(server).put('/').send({
             created: '2018-07-20T05:17:36Z',
             iid: '1',
-            type: '1',
+            type: 'applinth_enduser_app_hearthbeat',
             version: '1',
-            data: ['1'],
         });
         assert.deepEqual(resp.statusCode, 200);
         const storage = container.get<Storage>(Services.STORAGE);
@@ -25,10 +24,54 @@ describe('eventController', () => {
         assert.deepEqual(inserted, {
             _id: inserted?._id,
             created: '2018-07-20T05:17:36Z',
+            data: null,
+            iid: '1',
+            type: 'applinth_enduser_app_hearthbeat',
+            version: '1',
+        });
+    });
+    it('not supported event type', async () => {
+        const server = container.get<Express>(Services.SERVER);
+        const resp = await supertest(server).put('/').send({
+            created: '2018-07-20T05:17:36Z',
             iid: '1',
             type: '1',
             version: '1',
-            data: ['1'],
         });
+        assert.deepEqual(resp.statusCode, 400);
+        assert.deepEqual(resp.text, '{"msg":"Not supported event type!"}');
+    });
+    it('missing data field', async () => {
+        const server = container.get<Express>(Services.SERVER);
+        const resp = await supertest(server).put('/').send({
+            created: '2018-07-20T05:17:36Z',
+            iid: '1',
+            type: 'applinth_enduser_app_install',
+            version: '1',
+        });
+        assert.deepEqual(resp.statusCode, 400);
+        assert.deepEqual(resp.text, '{"msg":"Missing data field or required params in data for this type of Event!"}');
+    });
+    it('missing fields in data field', async () => {
+        const server = container.get<Express>(Services.SERVER);
+        const resp = await supertest(server).put('/').send({
+            created: '2018-07-20T05:17:36Z',
+            iid: '1',
+            type: 'applinth_enduser_app_install',
+            version: '1',
+            data: { aid: '1' },
+        });
+        assert.deepEqual(resp.statusCode, 400);
+        assert.deepEqual(resp.text, '{"msg":"Missing data field or required params in data for this type of Event!"}');
+
+        const resp2 = await supertest(server).put('/').send({
+            created: '2018-07-20T05:17:36Z',
+            iid: '1',
+            type: 'orchesty_operations',
+            version: '1',
+            data: { aid: '1', euid: '1' },
+        });
+        assert.deepEqual(resp2.statusCode, 400);
+        assert.deepEqual(resp2.text, '{"msg":"Missing data field or required params in data for this type of Event!"}');
     });
 });
