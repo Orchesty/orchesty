a1c42383bc3fd5af64b35cb51ba2ea854f723e30
Radek Jirsa <jirsa.r@hanaboso.com>
2024-07-29 10:30:52 +0200

    CI: Added custom dkr image mirror

---
+++ build/bin/clients-image-mirror.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+registry="dkr.hanaboso.net"
+
+clients='[
+    {"from": "dkr.hanaboso.net/pipes/pipes/comparator:master", "to": "dkr.hanaboso.net/oxy/images/comparator:master"},
+    {"from": "dkr.hanaboso.net/pipes/pipes/applinth:2.0", "to": "dkr.hanaboso.net/fabis/applinth/applinth:2.0"},
+    {"from": "dkr.hanaboso.net/pipes/pipes/applinth-marketplace-ui:2.0", "to": "dkr.hanaboso.net/fabis/applinth/applinth-marketplace-ui:2.0"}
+]'
+
+echo "$clients" | jq -c '.[]' | while read -r obj; do
+    from=$(echo "$obj" | jq -r '.from')
+    to=$(echo "$obj" | jq -r '.to')
+
+    imageFrom=${from//$registry/""}
+    imageTo=${to//$registry/""}
+
+    docker pull $registry$imageFrom
+    docker tag $registry$imageFrom $registry$imageTo
+    docker push $registry$imageTo
+done
