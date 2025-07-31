e7bbc091b97469a6edec53927267d9bb58ebd512
Radek Jirsa <jirsa.r@gmail.com>
2025-07-31 07:41:57 +0000

    Deps updated

--- src/HbPFLogsBundle/DependencyInjection/HbPFLogsExtension.php
+++ src/HbPFLogsBundle/DependencyInjection/HbPFLogsExtension.php
@@ -7,16 +7,14 @@ use Hanaboso\PipesFramework\HbPFLogsBundle\HbPFLogsBundle;
 use RuntimeException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Class HbPFLogsExtension
  *
  * @package Hanaboso\PipesFramework\HbPFLogsBundle\DependencyInjection
- *
- * @codeCoverageIgnore
  */
 final class HbPFLogsExtension extends Extension implements PrependExtensionInterface
 {
