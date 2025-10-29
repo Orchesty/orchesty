d2bd5b81c1fae94f90f6f2241e74eb8709f37fe8
Radek Bruha <bruha.r@hanaboso.com>
2025-10-29 12:30:50 +0100

    Counter: Update topology statistics

--- pkg/model/topology.go
+++ pkg/model/topology.go
@@ -1,7 +1,9 @@
 package model
 
+import "go.mongodb.org/mongo-driver/v2/bson"
+
 type Topology struct {
-	Id      string `bson:"_id"`
-	Name    string `bson:"name"`
-	Version int    `bson:"version"`
+	Id      bson.ObjectID `bson:"_id"`
+	Name    string        `bson:"name"`
+	Version int           `bson:"version"`
 }
