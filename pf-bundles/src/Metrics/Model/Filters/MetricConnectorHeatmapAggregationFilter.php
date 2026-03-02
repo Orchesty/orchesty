43ce4f20af523ed5b2d49b4103ad4c2fce3ac261
Radek Bruha <bruha.r@hanaboso.com>
2026-03-02 11:16:05 +0100

    PfBundles: Create configurable buckets size for some aggregation filters

--- pf-bundles/src/Metrics/Model/Filters/MetricConnectorHeatmapAggregationFilter.php
+++ pf-bundles/src/Metrics/Model/Filters/MetricConnectorHeatmapAggregationFilter.php
@@ -16,6 +16,18 @@ use Hanaboso\PipesFramework\Metrics\Document\ConnectorsMetrics;
 final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilterAbstract
 {
 
+    private int $bucketCount = 0;
+
+    /**
+     * @param int $buckets
+     *
+     * @return void
+     */
+    public function setBucketCount(int $buckets): void
+    {
+        $this->bucketCount = $buckets;
+    }
+
     /**
      * @return class-string
      */
@@ -69,6 +81,11 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
     ): void {
         $addConditionsCallback();
 
+        [$binSize, $gte, $lte] = AggregationFilterUtils::getDensifyBinSizeAndRangeFromAggregationBuilder(
+            $builder,
+            $this->bucketCount,
+        );
+
         $builder
             ->group()
             ->field('_id')
@@ -76,14 +93,22 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
                 $builder
                     ->expr()
                     ->field('hour')
-                    ->expression(
-                        $builder
-                            ->expr()
-                            ->dateTrunc(
-                                '$fields.created',
-                                'minute',
-                                AggregationFilterUtils::getDateTruncBinSizeFromAggregationBuilder($builder),
+                    ->toDate(
+                        $builder->expr()->add(
+                            $builder->expr()->toLong($gte),
+                            $builder->expr()->multiply(
+                                $builder->expr()->floor(
+                                    $builder->expr()->divide(
+                                        $builder->expr()->subtract(
+                                            $builder->expr()->toLong('$fields.created'),
+                                            $builder->expr()->toLong($gte),
+                                        ),
+                                        $binSize,
+                                    ),
+                                ),
+                                $binSize,
                             ),
+                        ),
                     )
                     ->field('nodeId')
                     ->expression('$tags.node_id'),
@@ -91,26 +116,22 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
             ->field('applicationId')
             ->first('$tags.application_id')
             ->field('success')
-            ->expression(
-                $builder->expr()->sum(
-                    $builder->expr()->cond(
-                        $builder->expr()->and(
-                            $builder->expr()->gte('$fields.response_code', 200),
-                            $builder->expr()->lte('$fields.response_code', 399),
-                        ),
-                        1,
-                        0,
+            ->sum(
+                $builder->expr()->cond(
+                    $builder->expr()->and(
+                        $builder->expr()->gte('$fields.response_code', 200),
+                        $builder->expr()->lte('$fields.response_code', 399),
                     ),
+                    1,
+                    0,
                 ),
             )
             ->field('failed')
-            ->expression(
-                $builder->expr()->sum(
-                    $builder->expr()->cond(
-                        $builder->expr()->gte('$fields.response_code', 400),
-                        1,
-                        0,
-                    ),
+            ->sum(
+                $builder->expr()->cond(
+                    $builder->expr()->gte('$fields.response_code', 400),
+                    1,
+                    0,
                 ),
             )
             ->addFields()
@@ -118,6 +139,29 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
             ->expression('$_id.nodeId')
             ->field('created')
             ->expression('$_id.hour');
+
+        if ($gte !== NULL && $lte !== NULL) {
+            $builder
+                ->densify('created')
+                ->partitionByFields('nodeId')
+                ->range([$gte, $lte], $binSize, 'millisecond')
+                ->fill()
+                ->partitionByFields('nodeId')
+                ->sortBy('created', 'asc')
+                ->output()
+                ->field('success')
+                ->value(0)
+                ->field('failed')
+                ->value(0)
+                ->field('applicationId')
+                ->locf()
+                ->fill()
+                ->partitionByFields('nodeId')
+                ->sortBy('created', 'desc')
+                ->output()
+                ->field('applicationId')
+                ->locf();
+        }
 
         $addSortationsCallback();
         $addPaginationCallback();
@@ -148,6 +192,11 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
     {
         $addConditionsCallback();
 
+        [$binSize, $gte, $lte] = AggregationFilterUtils::getDensifyBinSizeAndRangeFromAggregationBuilder(
+            $builder,
+            $this->bucketCount,
+        );
+
         $builder
             ->group()
             ->field('_id')
@@ -155,18 +204,38 @@ final class MetricConnectorHeatmapAggregationFilter extends GridAggregationFilte
                 $builder
                     ->expr()
                     ->field('hour')
-                    ->expression(
-                        $builder
-                            ->expr()
-                            ->dateTrunc(
-                                '$fields.created',
-                                'minute',
-                                AggregationFilterUtils::getDateTruncBinSizeFromAggregationBuilder($builder),
+                    ->toDate(
+                        $builder->expr()->add(
+                            $builder->expr()->toLong($gte),
+                            $builder->expr()->multiply(
+                                $builder->expr()->floor(
+                                    $builder->expr()->divide(
+                                        $builder->expr()->subtract(
+                                            $builder->expr()->toLong('$fields.created'),
+                                            $builder->expr()->toLong($gte),
+                                        ),
+                                        $binSize,
+                                    ),
+                                ),
+                                $binSize,
                             ),
+                        ),
                     )
                     ->field('nodeId')
                     ->expression('$tags.node_id'),
             );
+
+        if ($gte !== NULL && $lte !== NULL) {
+            $builder
+                ->addFields()
+                ->field('nodeId')
+                ->expression('$_id.nodeId')
+                ->field('created')
+                ->expression('$_id.hour')
+                ->densify('created')
+                ->partitionByFields('nodeId')
+                ->range([$gte, $lte], $binSize, 'millisecond');
+        }
     }
 
 }
