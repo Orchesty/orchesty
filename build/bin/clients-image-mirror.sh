86706f965e6f1f57a30ec2c280bbdb6b1eb1ab39
Radek Jirsa <jirsa.r@hanaboso.com>
2026-04-01 08:29:02 +0200

    Clients image mirror updated

--- build/bin/clients-image-mirror.sh
+++ build/bin/clients-image-mirror.sh
@@ -4,17 +4,15 @@ registry="dkr.hanaboso.net"
 
 clients='[
     {"from": "dkr.hanaboso.net/pipes/pipes/applinth:2.0", "to": "dkr.hanaboso.net/fabis/applinth/applinth:2.0"},
-    {"from": "dkr.hanaboso.net/pipes/pipes/applinth-marketplace-ui:2.0", "to": "dkr.hanaboso.net/fabis/applinth/applinth-marketplace-ui:2.0"}
+    {"from": "dkr.hanaboso.net/pipes/pipes/applinth-marketplace-ui:2.0", "to": "dkr.hanaboso.net/fabis/applinth/applinth-marketplace-ui:2.0"},
+    {"from": "dkr.hanaboso.net/pipes/pipes/frontend-enterprise:master", "to": "dkr.hanaboso.net/oxy/images/frontend-enterprise:3.0-rc1"},
+    {"from": "dkr.hanaboso.net/pipes/pipes/backend-enterprise:master", "to": "dkr.hanaboso.net/oxy/images/backend-enterprise:3.0-rc1"}
 ]'
 
 echo "$clients" | jq -c '.[]' | while read -r obj; do
     from=$(echo "$obj" | jq -r '.from')
     to=$(echo "$obj" | jq -r '.to')
 
-    imageFrom=${from//$registry/""}
-    imageTo=${to//$registry/""}
-
-    docker pull $registry$imageFrom
-    docker tag $registry$imageFrom $registry$imageTo
-    docker push $registry$imageTo
+    echo "Copying $from -> $to"
+    docker buildx imagetools create -t "$to" "$from"
 done
