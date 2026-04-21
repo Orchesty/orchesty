16827ac50fbf7bb01962fe37415eb379131d6655
Radek Bruha <bruha.r@hanaboso.com>
2026-04-21 12:19:27 +0200

    PfBundles: Add option to install tunnel worker via service command

--- tests/Integration/Command/InstallServiceCommandTest.php
+++ tests/Integration/Command/InstallServiceCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace PipesFrameworkTests\Integration\Command;
 
+use Hanaboso\PipesFramework\Configurator\Document\Sdk;
 use Hanaboso\PipesFramework\HbPFConfiguratorBundle\Command\InstallServiceCommand;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PipesFrameworkTests\DatabaseTestCaseAbstract;
@@ -23,21 +24,79 @@ final class InstallServiceCommandTest extends DatabaseTestCaseAbstract
      */
     public function testExecute(): void
     {
-        /** @var KernelInterface $kernel */
-        $kernel      = self::$kernel;
-        $application = new Application($kernel);
-        $command     = $application->get('service:install');
-
-        $commandTester = new CommandTester($command);
+        $commandTester = $this->createCommandTester();
         $commandTester->execute(
             [
-                'command' => $command->getName(),
-                'name'    => '',
+                'command' => 'service:install',
+                'name'    => 'http-sdk',
+                'url'     => 'http-sdk:8080',
+            ],
+        );
+
+        self::assertStringContainsString('Done!', $commandTester->getDisplay());
+        self::assertSame(0, $commandTester->getStatusCode());
+
+        /** @var Sdk|null $sdk */
+        $sdk = $this->dm->getRepository(Sdk::class)->findOneBy([Sdk::NAME => 'http-sdk']);
+        self::assertNotNull($sdk);
+        self::assertSame(Sdk::TYPE_HTTP, $sdk->getType());
+    }
+
+    /**
+     * @return void
+     */
+    public function testExecuteTunnel(): void
+    {
+        $commandTester = $this->createCommandTester();
+        $commandTester->execute(
+            [
+                'command' => 'service:install',
+                'name'    => 'node-sdk-tunnel',
+                'type'    => Sdk::TYPE_TUNNEL,
                 'url'     => '',
             ],
         );
 
         self::assertStringContainsString('Done!', $commandTester->getDisplay());
+        self::assertSame(0, $commandTester->getStatusCode());
+
+        /** @var Sdk|null $sdk */
+        $sdk = $this->dm->getRepository(Sdk::class)->findOneBy([Sdk::NAME => 'node-sdk-tunnel']);
+        self::assertNotNull($sdk);
+        self::assertSame(Sdk::TYPE_TUNNEL, $sdk->getType());
+        self::assertSame('', $sdk->getUrl());
+    }
+
+    /**
+     * @return void
+     */
+    public function testExecuteInvalidType(): void
+    {
+        $commandTester = $this->createCommandTester();
+        $commandTester->execute(
+            [
+                'command' => 'service:install',
+                'name'    => 'bad-sdk',
+                'type'    => 'bad-type',
+                'url'     => 'bad-sdk:8080',
+            ],
+        );
+
+        self::assertStringContainsString('Invalid type', $commandTester->getDisplay());
+        self::assertSame(1, $commandTester->getStatusCode());
+        self::assertCount(0, $this->dm->getRepository(Sdk::class)->findAll());
+    }
+
+    /**
+     * @return CommandTester
+     */
+    private function createCommandTester(): CommandTester
+    {
+        /** @var KernelInterface $kernel */
+        $kernel      = self::$kernel;
+        $application = new Application($kernel);
+
+        return new CommandTester($application->get('service:install'));
     }
 
 }
